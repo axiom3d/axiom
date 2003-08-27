c9ffdc5a05b27c55fe6b4ec6c580ea5288183bee
leedgitar <leedgitar@bb9bf1cf-960d-0410-9a3a-baeba60cf407>
2003-08-27 03:26:00 +0000

    Updated SharpDevelop project with recent file removals.

--- Axiom/Engine/Utility/ConfigDialog.cs
+++ Axiom/Engine/Utility/ConfigDialog.cs
@@ -1,4 +1,4 @@
-﻿#region LGPL License
+#region LGPL License
 /*
 Axiom Game Engine Library
 Copyright (C) 2003  Axiom Project Team
@@ -41,11 +41,11 @@ namespace Axiom.Utility
 	/// </summary>
 	public class ConfigDialog : System.Windows.Forms.Form
 	{
-		private System.Windows.Forms.Button btnOK;
+		private System.Windows.Forms.ComboBox cboRenderSystems;
 		private System.Windows.Forms.ComboBox cboResolution;
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.GroupBox groupBox1;
-		private System.Windows.Forms.ComboBox cboRenderSystems;
+		private System.Windows.Forms.Button btnOK;
 		private System.Windows.Forms.CheckBox chkFullScreen;
 		private System.Windows.Forms.PictureBox pictureBox1;
 		private System.Windows.Forms.Button btnCancel;
@@ -87,19 +87,20 @@ namespace Axiom.Utility
 		/// Required method for Designer support - do not modify
 		/// the contents of this method with the code editor.
 		/// </summary>
+		
 		private void InitializeComponent() {
 			System.Resources.ResourceManager resources = new System.Resources.ResourceManager(typeof(ConfigDialog));
 			this.lblResolutions = new System.Windows.Forms.Label();
 			this.btnCancel = new System.Windows.Forms.Button();
 			this.pictureBox1 = new System.Windows.Forms.PictureBox();
 			this.chkFullScreen = new System.Windows.Forms.CheckBox();
-			this.cboRenderSystems = new System.Windows.Forms.ComboBox();
+			this.btnOK = new System.Windows.Forms.Button();
 			this.groupBox1 = new System.Windows.Forms.GroupBox();
 			this.label1 = new System.Windows.Forms.Label();
 			this.cboResolution = new System.Windows.Forms.ComboBox();
-			this.btnOK = new System.Windows.Forms.Button();
-			this.SuspendLayout();
+			this.cboRenderSystems = new System.Windows.Forms.ComboBox();
 			this.groupBox1.SuspendLayout();
+			this.SuspendLayout();
 			// 
 			// lblResolutions
 			// 
@@ -136,6 +137,7 @@ namespace Axiom.Utility
 			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
 			this.pictureBox1.TabIndex = 3;
 			this.pictureBox1.TabStop = false;
+			
 			// 
 			// chkFullScreen
 			// 
@@ -150,24 +152,28 @@ namespace Axiom.Utility
 			this.chkFullScreen.TabIndex = 0;
 			this.chkFullScreen.Text = "Fullscreen?";
 			// 
-			// cboRenderSystems
+			// btnOK
 			// 
-			this.cboRenderSystems.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-			this.cboRenderSystems.Font = new System.Drawing.Font("Palatino Linotype", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
-			this.cboRenderSystems.Location = new System.Drawing.Point(144, 32);
-			this.cboRenderSystems.Name = "cboRenderSystems";
-			this.cboRenderSystems.Size = new System.Drawing.Size(176, 24);
-			this.cboRenderSystems.TabIndex = 8;
-			this.cboRenderSystems.SelectedIndexChanged += new System.EventHandler(this.cboRenderSystems_SelectedIndexChanged);
+			this.btnOK.BackColor = System.Drawing.SystemColors.Control;
+			this.btnOK.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+			this.btnOK.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnOK.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
+			this.btnOK.ForeColor = System.Drawing.Color.Black;
+			this.btnOK.Location = new System.Drawing.Point(109, 296);
+			this.btnOK.Name = "btnOK";
+			this.btnOK.Size = new System.Drawing.Size(96, 24);
+			this.btnOK.TabIndex = 2;
+			this.btnOK.Text = "Start";
+			this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
 			// 
 			// groupBox1
 			// 
-			this.groupBox1.Controls.AddRange(new System.Windows.Forms.Control[] {
-						this.label1,
-						this.cboRenderSystems,
-						this.lblResolutions,
-						this.cboResolution,
-						this.chkFullScreen});
+			this.groupBox1.Controls.Add(this.label1);
+			this.groupBox1.Controls.Add(this.cboRenderSystems);
+			this.groupBox1.Controls.Add(this.lblResolutions);
+			this.groupBox1.Controls.Add(this.cboResolution);
+			this.groupBox1.Controls.Add(this.chkFullScreen);
 			this.groupBox1.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
 			this.groupBox1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
 			this.groupBox1.Location = new System.Drawing.Point(24, 136);
@@ -196,20 +202,15 @@ namespace Axiom.Utility
 			this.cboResolution.Size = new System.Drawing.Size(176, 24);
 			this.cboResolution.TabIndex = 6;
 			// 
-			// btnOK
+			// cboRenderSystems
 			// 
-			this.btnOK.BackColor = System.Drawing.SystemColors.Control;
-			this.btnOK.Cursor = System.Windows.Forms.Cursors.Hand;
-			this.btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
-			this.btnOK.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-			this.btnOK.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
-			this.btnOK.ForeColor = System.Drawing.Color.Black;
-			this.btnOK.Location = new System.Drawing.Point(109, 296);
-			this.btnOK.Name = "btnOK";
-			this.btnOK.Size = new System.Drawing.Size(96, 24);
-			this.btnOK.TabIndex = 2;
-			this.btnOK.Text = "Start";
-			this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+			this.cboRenderSystems.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cboRenderSystems.Font = new System.Drawing.Font("Palatino Linotype", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
+			this.cboRenderSystems.Location = new System.Drawing.Point(144, 32);
+			this.cboRenderSystems.Name = "cboRenderSystems";
+			this.cboRenderSystems.Size = new System.Drawing.Size(176, 24);
+			this.cboRenderSystems.TabIndex = 8;
+			this.cboRenderSystems.SelectedIndexChanged += new System.EventHandler(this.cboRenderSystems_SelectedIndexChanged);
 			// 
 			// ConfigDialog
 			// 
@@ -218,15 +219,15 @@ namespace Axiom.Utility
 			this.CancelButton = this.btnCancel;
 			this.ClientSize = new System.Drawing.Size(442, 344);
 			this.ControlBox = false;
-			this.Controls.AddRange(new System.Windows.Forms.Control[] {
-						this.groupBox1,
-						this.pictureBox1,
-						this.btnOK,
-						this.btnCancel});
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.btnOK);
+			this.Controls.Add(this.btnCancel);
 			this.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
 			this.MaximizeBox = false;
 			this.MinimizeBox = false;
+			this.Name = "ConfigDialog";
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "Axiom Game Engine";
 			this.Load += new System.EventHandler(this.ConfigDialog_Load);
