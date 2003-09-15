57e89e2b083e23ed67490ce164b5675b66900c6b
leedgitar <12863619+leedgitar@users.noreply.github.com>
2003-09-15 02:02:42 +0000

    Initial checking of Gui WIP.

---
+++ Axiom/Engine/Gui/GuiContainer.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace Axiom.Gui
+{
+	/// <summary>
+	/// 	Summary description for GuiContainer.
+	/// </summary>
+	public class GuiContainer
+	{
+		#region Member variables
+		
+		#endregion
+		
+		#region Constructors
+		
+		public GuiContainer()
+		{
+			//
+			// TODO: Add constructor logic here
+			//
+		}
+		
+		#endregion
+		
+		#region Methods
+		
+		#endregion
+		
+		#region Properties
+		
+		#endregion
+
+	}
+}
