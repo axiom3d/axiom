2c55b56ab75a7fe84454628cea5e3d2d08f23f94
leedgitar <leedgitar@bb9bf1cf-960d-0410-9a3a-baeba60cf407>
2003-09-20 18:51:04 +0000

    Updated SkyBox with particles.

--- Axiom/DemoTest/Demos/SkyBox.cs
+++ Axiom/DemoTest/Demos/SkyBox.cs
@@ -25,46 +25,104 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 #endregion
 
 using System;
+using System.Drawing;
+using System.Windows.Forms;
 using Axiom.Core;
 using Axiom.MathLib;
+using Axiom.ParticleSystems;
 using Axiom.Utility;
 
 namespace Demos {
-    /// <summary>
-    /// 	Summary description for SkyBox.
-    /// </summary>
-    public class SkyBox : TechDemo {	
+    public class SkyBox : TechDemo {
+        #region Fields
+        private float defaultDimension = 25;
+        private float defaultVelocity = 50;
+        protected ParticleSystem thrusters = null;
+        #endregion Fields
+
+        protected override bool OnFrameStarted(Object source, FrameEventArgs e) {
+            base.OnFrameStarted (source, e);
+
+            if(input.IsKeyPressed(Keys.N)) {
+                thrusters.DefaultWidth = defaultDimension + 0.25f;
+                thrusters.DefaultHeight = defaultDimension + 0.25f;
+                defaultDimension += 0.25f;
+            }
+
+            if(input.IsKeyPressed(Keys.M)) {
+                thrusters.DefaultWidth = defaultDimension - 0.25f;
+                thrusters.DefaultHeight = defaultDimension - 0.25f;
+                defaultDimension -= 0.25f;
+            }
+
+            if(input.IsKeyPressed(Keys.H)) {
+                thrusters.GetEmitter(0).ParticleVelocity = defaultVelocity + 1;
+                thrusters.GetEmitter(1).ParticleVelocity = defaultVelocity + 1;
+                defaultVelocity += 1;
+            }
+
+            if(input.IsKeyPressed(Keys.J) && !(defaultVelocity < 0.0f)) {
+                thrusters.GetEmitter(0).ParticleVelocity = defaultVelocity - 1;
+                thrusters.GetEmitter(1).ParticleVelocity = defaultVelocity - 1;
+                defaultVelocity -= 1;
+            }
+
+            return true;
+        }
+
         #region Methods
-		
         protected override void CreateScene() {
             // since whole screen is being redrawn every frame, dont bother clearing
             // option works for GL right now, uncomment to test it out.  huge fps increase
             // also, depth_write in the skybox material must be set to on
             //mainViewport.ClearEveryFrame = false;
 
-            // set some ambient light
+            // set ambient light
             scene.AmbientLight = new ColorEx(1.0f, 0.5f, 0.5f, 0.5f);
 
-            Plane plane = new Plane();
-            // 5000 units from the camera
-            plane.D = 5000;
-            // above the camera, facing down
-            plane.Normal = -Vector3.UnitY;
-
-            // create the skybox
+            // create a skybox
             scene.SetSkyBox(true, "Skybox/Space", 50);
 
-            // create a default point light
+            // create a light
             Light light = scene.CreateLight("MainLight");
             light.Position = new Vector3(20, 80, 50);
 
-            // stuff a dragon into the scene
-            Entity entity = scene.CreateEntity("razor", "razor.mesh");
-            scene.RootSceneNode.AttachObject(entity);			
+            // add a nice starship
+            Entity ship = scene.CreateEntity("razor", "razor.mesh");
+            scene.RootSceneNode.AttachObject(ship);
 
-            // TODO: Add particle system thrusters after post-VBO particles are implemented
+            thrusters = ParticleSystemManager.Instance.CreateSystem("ParticleSystem", 200);
+            thrusters.MaterialName = "Particles/Flare";
+            thrusters.DefaultWidth = 25;
+            thrusters.DefaultHeight = 25;
+
+            ParticleEmitter emitter1 = thrusters.AddEmitter("Point");
+            ParticleEmitter emitter2 = thrusters.AddEmitter("Point");
+
+            // thruster 1
+            emitter1.Angle = 3;
+            emitter1.TimeToLive = 0.2f;
+            emitter1.EmissionRate = 70;
+            emitter1.ParticleVelocity = 50;
+            emitter1.Direction = -Vector3.UnitZ;
+            emitter1.ColorRangeStart = ColorEx.FromColor(Color.White);
+            emitter1.ColorRangeEnd = ColorEx.FromColor(Color.Red);
+
+            // thruster 2
+            emitter2.Angle = 3;
+            emitter2.TimeToLive = 0.2f;
+            emitter2.EmissionRate = 70;
+            emitter2.ParticleVelocity = 50;
+            emitter2.Direction = -Vector3.UnitZ;
+            emitter2.ColorRangeStart = ColorEx.FromColor(Color.White);
+            emitter2.ColorRangeEnd = ColorEx.FromColor(Color.Red);
+
+            // set the position of the thrusters
+            emitter1.Position = new Vector3(5.7f, 0, 0);
+            emitter2.Position = new Vector3(-18, 0, 0);
+
+            ((SceneNode) scene.RootSceneNode.CreateChild(new Vector3(0, 6.5f, -67), Quaternion.Identity)).AttachObject(thrusters);
         }
-
         #endregion
     }
 }
