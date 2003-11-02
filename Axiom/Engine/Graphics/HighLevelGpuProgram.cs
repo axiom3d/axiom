cc0395e70cbf070b01baa1f7e77850dabdafbd66
leedgitar <12863619+leedgitar@users.noreply.github.com>
2003-11-02 02:15:14 +0000

    Moved Axiom.SubSystems.Rendering to Axiom.Graphics

---
+++ Axiom/Engine/Graphics/HighLevelGpuProgram.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace Axiom.Graphics
+{
+	/// <summary>
+	/// 	Summary description for HighLevelGpuProgram.
+	/// </summary>
+	public class HighLevelGpuProgram
+	{
+		#region Member variables
+		
+		#endregion
+		
+		#region Constructors
+		
+		public HighLevelGpuProgram()
+		{
+			//
+			// TODO: Add constructor logic here
+			//
+		}
+		
+		#endregion
+		
+		#region Methods
+		
+		#endregion
+		
+		#region Properties
+		
+		#endregion
+
+	}
+}
---
+++ Axiom/Engine/Graphics/HighLevelGpuProgramManager.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace Axiom.Graphics
+{
+	/// <summary>
+	/// 	Summary description for HighLevelGpuProgramManager.
+	/// </summary>
+	public class HighLevelGpuProgramManager
+	{
+		#region Member variables
+		
+		#endregion
+		
+		#region Constructors
+		
+		public HighLevelGpuProgramManager()
+		{
+			//
+			// TODO: Add constructor logic here
+			//
+		}
+		
+		#endregion
+		
+		#region Methods
+		
+        //public void AddCompiler(string type, 
+
+		#endregion
+		
+		#region Properties
+		
+		#endregion
+
+	}
+}
