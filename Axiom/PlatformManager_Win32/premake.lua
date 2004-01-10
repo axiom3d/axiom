0e0648e931e1e9fd4be2ef9f168d9283f7f35429
leedgitar <leedgitar@bb9bf1cf-960d-0410-9a3a-baeba60cf407>
2004-01-10 04:16:23 +0000

    Adding the Win32 platform manager to the repository.

---
+++ Axiom/PlatformManager_Win32/premake.lua
@@ -0,0 +1,15 @@
+package.name = "PlatformManager_Win32"
+package.language = "c#"
+package.kind = "dll"
+package.target = "Axiom.Platforms.Win32"
+package.links = { "System", "System.Drawing", "System.Windows.Forms", "Engine" }
+
+if(target == "sd") then
+	tinsert(package.links, "Microsoft.DirectX, Version=1.0.1901.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35")
+	tinsert(package.links, "Microsoft.DirectX.DirectInput, Version=1.0.1901.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35")
+else
+	tinsert(package.links, "Microsoft.DirectX")
+	tinsert(package.links, "Microsoft.DirectX.DirectInput")
+end
+
+package.files = { matchfiles("*.cs") }
