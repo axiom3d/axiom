90c2026f1b9a244947bbce8a7ca7ed1429cb6fa9
leedgitar <leedgitar@bb9bf1cf-960d-0410-9a3a-baeba60cf407>
2004-02-01 19:23:34 +0000

    Updated SharpDevelop files.

--- Axiom/premake.lua
+++ Axiom/premake.lua
@@ -7,6 +7,7 @@ dopackage("DynamicsSystem_ODE")
 dopackage("Plugin_CgProgramManager")
 dopackage("Plugin_GuiElements")
 dopackage("RenderSystem_OpenGL")
+dopackage("Plugin_OctreeSceneManager")
 
 if(OS == "windows") then
 	dopackage("RenderSystem_DirectX9")
