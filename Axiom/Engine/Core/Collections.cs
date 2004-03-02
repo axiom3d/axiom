bf9833dc41e79d3be96b965cbb50c0415907f788
leedgitar <12863619+leedgitar@users.noreply.github.com>
2004-03-02 05:50:14 +0000

    Added a few missing files.

---
+++ Axiom/Engine/Core/Collections.cs
@@ -0,0 +1,8 @@
+using System;
+using System.Collections;
+
+namespace Axiom.Core {
+    public class PluginList : ArrayList {}
+
+    public class MeshLodUsageList : ArrayList {}
+}
---
+++ Axiom/Engine/Graphics/Collections.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+
+namespace Axiom.Graphics {
+    public class VertexElementList : ArrayList {}
+
+    public class TextureEffectList : ArrayList {}
+
+    public class RenderTargetList : ArrayList {}
+
+    public class PassList : ArrayList {}
+
+    public class TechniqueList : ArrayList {}
+
+    public class TextureUnitStateList : ArrayList {}
+
+    public class AutoConstantEntryList : ArrayList {}
+
+    public class RenderableList : ArrayList {}
+}
