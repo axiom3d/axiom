3a18eaec23a5060d09b7da666525d10809f7adbd
leedgitar <12863619+leedgitar@users.noreply.github.com>
2004-03-05 04:02:46 +0000

    Updates.

--- Axiom/Engine/Graphics/EdgeListBuilder.cs
+++ Axiom/Engine/Graphics/EdgeListBuilder.cs
@@ -25,13 +25,47 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 #endregion
 
 using System;
+using Axiom.MathLib;
 
 namespace Axiom.Graphics {
 	/// <summary>
-	/// Summary description for EdgeListBuilder.
+	///     General utility class for building edge lists for geometry.
 	/// </summary>
+	/// <remarks>
+	///     You can add multiple sets of vertex and index data to build and edge list. 
+	///     Edges will be built between the various sets as well as within sets; this allows 
+	///     you to use a model which is built from multiple SubMeshes each using 
+	///     separate index and (optionally) vertex data and still get the same connectivity 
+	///     information. It's important to note that the indexes for the edge will be constrained
+	///     to a single vertex buffer though (this is required in order to render the edge).
+	/// </remarks>
 	public class EdgeListBuilder {
 		public EdgeListBuilder() {
 		}
+
+        #region Structs
+
+        /// <summary>
+        ///     A vertex can actually represent several vertices in the final model, because
+        ///     vertices along texture seams etc will have been duplicated. In order to properly
+        ///     evaluate the surface properties, a single common vertex is used for these duplicates,
+        ///     and the faces hold the detail of the duplicated vertices.
+        /// </summary>
+        public struct CommonVertex {
+            /// <summary>
+            ///     Location of point in euclidean space.
+            /// </summary>
+            public Vector3 position;
+            /// <summary>
+            ///     Place of vertex in original vertex set.
+            /// </summary>
+            public int index;
+            /// <summary>
+            ///      The vertex set this came from.
+            /// </summary>
+            public int vertexSet;
+        };
+
+        #endregion Structs
 	}
 }
