53ca4a5376cedcda224c8d360a510ebbd4aaab46
leedgitar <leedgitar@bb9bf1cf-960d-0410-9a3a-baeba60cf407>
2004-04-27 01:51:54 +0000

    - Patch from Antiarc (Chris Heald) to re-add multiple RenderWindow support for Direct3D.  The patch also fixes a memork leak in SetViewport.

--- Axiom/RenderSystem_DirectX9/D3DWindow.cs
+++ Axiom/RenderSystem_DirectX9/D3DWindow.cs
@@ -33,251 +33,278 @@ using Microsoft.DirectX.Direct3D;
 using D3D = Microsoft.DirectX.Direct3D;
 
 namespace Axiom.RenderSystems.DirectX9 {
-    /// <summary>
-    /// The Direct3D implementation of the RenderWindow class.
-    /// </summary>
-    public class D3DWindow : RenderWindow {
-        /// <summary>A handle to the Direct3D device of the DirectX9RenderSystem.</summary>
-        private D3D.Device device;
-        /// <summary>Used to provide support for multiple RenderWindows per device.</summary>
-        private D3D.Surface backBuffer;
-        private D3D.Surface stencilBuffer;
+	/// <summary>
+	/// The Direct3D implementation of the RenderWindow class.
+	/// </summary>
+	public class D3DWindow : RenderWindow {
+		/// <summary>A handle to the Direct3D device of the DirectX9RenderSystem.</summary>
+		private D3D.Device device;
+		/// <summary>Used to provide support for multiple RenderWindows per device.</summary>
+		private D3D.Surface backBuffer;
+		private D3D.Surface stencilBuffer;
+		private D3D.SwapChain swapChain;
+	
+		#region Constructor
+	
+		public D3DWindow() {
+		}
+	
+		#endregion
+	
+		#region RenderWindow implementation
+	
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="name"></param>
+		/// <param name="width"></param>
+		/// <param name="height"></param>
+		/// <param name="colorDepth"></param>
+		/// <param name="isFullScreen"></param>
+		/// <param name="left"></param>
+		/// <param name="top"></param>
+		/// <param name="depthBuffer"></param>height
+		/// <param name="miscParams"></param>
+		public override void Create(string name, int width, int height, int colorDepth, bool isFullScreen, int left, int top, bool depthBuffer, params object[] miscParams) {
+			// mMiscParams[0] = Direct3D.Device
+			// mMiscParams[1] = D3DRenderSystem.Driver
+			// mMiscParams[2] = Axiom.Core.RenderWindow
+ 		
+			Driver driver = null;
+			Control targetControl = null;
 
-        #region Constructor
-
-        public D3DWindow() {
-        }
-
-        #endregion
-
-        #region RenderWindow implementation
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="name"></param>
-        /// <param name="width"></param>
-        /// <param name="height"></param>
-        /// <param name="colorDepth"></param>
-        /// <param name="isFullScreen"></param>
-        /// <param name="left"></param>
-        /// <param name="top"></param>
-        /// <param name="depthBuffer"></param>height
-        /// <param name="miscParams"></param>
-        public override void Create(string name, int width, int height, int colorDepth, bool isFullScreen, int left, int top, bool depthBuffer, params object[] miscParams) {
-            // mMiscParams[0] = Direct3D.Device
-            // mMiscParams[1] = D3DRenderSystem.Driver
-            // mMiscParams[2] = Axiom.Core.RenderWindow
-		
-            Driver driver = null;
-            Control targetControl = null;
-
-            /// get the Direct3D.Device params
-            if(miscParams.Length > 0)
-                targetControl = (System.Windows.Forms.Control)miscParams[0];
-
-            PresentParameters presentParams = new PresentParameters();
-
-            presentParams.Windowed = !isFullScreen;
-            presentParams.BackBufferCount = 1;
-            presentParams.EnableAutoDepthStencil = depthBuffer;
-            presentParams.BackBufferWidth = width;
-            presentParams.BackBufferHeight = height;
-            presentParams.MultiSample = MultiSampleType.None;
-            presentParams.SwapEffect = SwapEffect.Discard;
-            // TODO: Check vsync setting
-            presentParams.PresentationInterval = PresentInterval.Immediate;
-
-            // supports 16 and 32 bit color
-			if(colorDepth == 16) {
-				presentParams.BackBufferFormat = Format.R5G6B5;
+			/// get the Direct3D.Device params
+			if(miscParams.Length > 0) {
+				targetControl = (System.Windows.Forms.Control)miscParams[0];
 			}
+
+			// CMH - 4/24/2004 - Start
+			if(miscParams.Length > 1 && miscParams[1] != null) {
+				device = (Device)miscParams[1];
+			}
+			if(device == null) {
+				throw new Exception("Error creating DirectX window: device is null.");
+			}
+
+			// CMH - End
+	       
+			device.DeviceReset += new EventHandler(OnResetDevice);
+			this.OnResetDevice(device, null);
+			device.DeviceLost += new EventHandler(device_DeviceLost);
+			
+
+			/*
+			 * CMH 4/24/2004 - Note: The device initialization code has been moved to initDevice()
+			 * in D3D9RenderSystem.cs, as we don't want to init a new device with every window.
+			 */
+			
+
+			// CMH - 4/24/2004 - Start
+			
+			/* If we're in fullscreen, we can use the device's back and stencil buffers.
+			 * If we're in windowed mode, we'll want our own.
+			 * get references to the render target and depth stencil surface
+			 */
+			if(isFullScreen) {
+				backBuffer = device.GetRenderTarget(0);
+				stencilBuffer = device.DepthStencilSurface;
+			} 
 			else {
-				presentParams.BackBufferFormat = Format.X8R8G8B8;
+				PresentParameters presentParams = new PresentParameters(device.PresentationParameters);
+				presentParams.Windowed = true;
+				presentParams.BackBufferCount = 1;
+				presentParams.EnableAutoDepthStencil = depthBuffer;
+				presentParams.SwapEffect = SwapEffect.Discard;
+				presentParams.DeviceWindow = targetControl;
+				presentParams.BackBufferHeight = height;
+				presentParams.BackBufferWidth = width;
+				swapChain = new SwapChain(device, presentParams);
+				customAttributes["SwapChain"] = swapChain;
+				
+				stencilBuffer = device.CreateDepthStencilSurface(
+					width, height,
+					device.PresentationParameters.AutoDepthStencilFormat,
+					device.PresentationParameters.MultiSample,
+					device.PresentationParameters.MultiSampleQuality, 
+					false);
 			}
+			// CMH - End
 
-            if(colorDepth > 16) {
-                // check for 24 bit Z buffer with 8 bit stencil (optimal choice)
-                if(!D3D.Manager.CheckDeviceFormat(0, DeviceType.Hardware, presentParams.BackBufferFormat, Usage.DepthStencil, ResourceType.Surface, DepthFormat.D24S8)) {
-                    // doh, check for 32 bit Z buffer then
-                    if(!D3D.Manager.CheckDeviceFormat(0, DeviceType.Hardware, presentParams.BackBufferFormat, Usage.DepthStencil, ResourceType.Surface, DepthFormat.D32)) {
-                        // float doh, just use 16 bit Z buffer
-                        presentParams.AutoDepthStencilFormat = DepthFormat.D16;
-                    }
-                    else {
-                        // use 32 bit Z buffer
-                        presentParams.AutoDepthStencilFormat = DepthFormat.D32;
-                    }
-                }
-                else {
-					// <flair>Woooooooooo!</flair>
-					if(D3D.Manager.CheckDepthStencilMatch(0, DeviceType.Hardware, presentParams.BackBufferFormat, presentParams.BackBufferFormat, D3D.DepthFormat.D24S8)) {
-						presentParams.AutoDepthStencilFormat = DepthFormat.D24S8;
+			// set the params of the window
+			this.Name = name;
+			this.colorDepth = colorDepth;
+			this.width = width;
+			this.height = height;
+			this.isFullScreen = isFullScreen;
+			this.top = top;
+			this.left = left;
+
+			// set as active
+			this.isActive = true;
+		}
+
+		public override object GetCustomAttribute(string attribute) {
+			switch(attribute) {
+				case "D3DDEVICE":
+					return device;
+
+				case "D3DZBUFFER":
+					return stencilBuffer;
+
+				case "D3DBACKBUFFER":
+					// CMH - 4/24/2004 - Start
+
+					// if we're in windowed mode, we want to get our own backbuffer.
+					if(isFullScreen) {
+						return backBuffer;
 					}
 					else {
-						presentParams.AutoDepthStencilFormat = DepthFormat.D24X8;
+						return swapChain.GetBackBuffer(0, BackBufferType.Mono);
 					}
-                }
-            }
-            else {
-                // use 16 bit Z buffer if they arent using true color
-                presentParams.AutoDepthStencilFormat = DepthFormat.D16;
-            }
+					// CMH - End
+			}
 
-            // create the D3D Device, trying for the best vertex support first, and settling for less if necessary
-            try {
-                // hardware vertex processing
-                device = new D3D.Device(0, DeviceType.Hardware, targetControl, CreateFlags.HardwareVertexProcessing, presentParams);
-            }
-            catch(Exception) {
-                try {
-                    // doh, how bout mixed vertex processing
-                    device = new D3D.Device(0, DeviceType.Hardware, targetControl, CreateFlags.MixedVertexProcessing, presentParams);
-                }
-                catch(Exception) {
-                    // what the...ok, how bout software vertex procssing.  if this fails, then I don't even know how they are seeing
-                    // anything at all since they obviously don't have a video card installed
-                    device = new D3D.Device(0, DeviceType.Hardware, targetControl, CreateFlags.SoftwareVertexProcessing, presentParams);
-                }
-            }
+			return new NotSupportedException("There is no D3D RenderWindow custom attribute named " + attribute);
+		}
 
-            device.DeviceReset += new EventHandler(OnResetDevice);
-            this.OnResetDevice(device, null);
-            device.DeviceLost += new EventHandler(device_DeviceLost);
+		/// <summary>
+		/// 
+		/// </summary>
+		public override void Destroy() {
+			// if the control is a form, then close it
+			if(targetHandle is System.Windows.Forms.Form) {
+				Form form = targetHandle as System.Windows.Forms.Form;
+				form.Close();
+			}
 
-            // get references to the render target and depth stencil surface
-            backBuffer = device.GetRenderTarget(0);
-            stencilBuffer = device.DepthStencilSurface;
+			// make sure this window is no longer active
+			isActive = false;
+		}
 
-            // set the params of the window
-            this.Name = name;
-            this.colorDepth = colorDepth;
-            this.width = width;
-            this.height = height;
-            this.isFullScreen = isFullScreen;
-            this.top = top;
-            this.left = left;
+		public override void Reposition(int left, int right) {
+			// TODO: Implementation of D3DWindow.Reposition()
+		}
 
-            // set as active
-            this.isActive = true;
-        }
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="width"></param>
+		/// <param name="height"></param>
+		public override void Resize(int width, int height) {
+			// CMH 4/24/2004 - Start
+			width = width < 10 ? 10 : width;
+			height = height < 10 ? 10 : height;
+			this.height = height;
+			this.width = width;
 
-        public override object GetCustomAttribute(string attribute) {
-            switch(attribute) {
-                case "D3DDEVICE":
-                    return device;
-                case "D3DZBUFFER":
-                    return stencilBuffer;
-                case "D3DBACKBUFFER":
-                    return backBuffer;
-            }
+			if(!isFullScreen) {
+				PresentParameters p = new PresentParameters(swapChain.PresentParamters);
+				p.BackBufferHeight = height;
+				p.BackBufferWidth = width;
+				swapChain.Dispose();
+				swapChain =  new SwapChain(device, p);
+				stencilBuffer.Dispose();
+				stencilBuffer = device.CreateDepthStencilSurface(
+					width, height,
+					device.PresentationParameters.AutoDepthStencilFormat,
+					device.PresentationParameters.MultiSample,
+					device.PresentationParameters.MultiSampleQuality, 
+					false);
 
-            return new NotSupportedException("There is no D3D RenderWindow custom attribute named " + attribute);
-        }
+				customAttributes["SwapChain"] = swapChain;
+			}
+			// CMH - End
+		}
 
-        /// <summary>
-        /// 
-        /// </summary>
-        public override void Destroy() {
-//            // if the control is a form, then close it
-            if(targetHandle is System.Windows.Forms.Form) {
-                Form form = targetHandle as System.Windows.Forms.Form;
-                form.Close();
-            }
-//            else {
-//                if(control.Parent != null) {
-//                    Form form = (Form)control.Parent;
-//                    form.Close();
-//                }
-//            }
-
-            // make sure this window is no longer active
-            this.isActive = false;
-        }
-
-        public override void Reposition(int left, int right) {
-            // TODO: Implementation of D3DWindow.Reposition()
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="width"></param>
-        /// <param name="height"></param>
-        public override void Resize(int width, int height) {
-            // TODO: Implementation of D3DWindow.Resize()
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="waitForVSync"></param>
-        public override void SwapBuffers(bool waitForVSync) {
-            try {
-                // tests coop level to make sure we are ok to render
-                device.TestCooperativeLevel();
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="waitForVSync"></param>
+		public override void SwapBuffers(bool waitForVSync) {
+			try {
+				// tests coop level to make sure we are ok to render
+				device.TestCooperativeLevel();
 				
-                // swap back buffer to the front
-                device.Present();
-            }
-            catch(DeviceLostException dlx) {
-                Console.WriteLine(dlx.ToString());
-            }
-            catch(DeviceNotResetException dnrx) {
-                Console.WriteLine(dnrx.ToString());
-                device.Reset(device.PresentationParameters);
-            }
-        }
+				// swap back buffer to the front
+				// CMH 4/24/2004 - Start
+				if(this.isFullScreen) {
+					device.Present();
+				}
+				else {
+					swapChain.Present();
+				}
+				// CMH - End
+			}
+			catch(DeviceLostException dlx) {
+				Console.WriteLine(dlx.ToString());
+			}
+			catch(DeviceNotResetException dnrx) {
+				Console.WriteLine(dnrx.ToString());
+				device.Reset(device.PresentationParameters);
+			}
+		}
 
-        /// <summary>
-        /// 
-        /// </summary>
-        public override bool IsActive {
-            get { 
+		/// <summary>
+		/// 
+		/// </summary>
+		public override bool IsActive {
+			get { 
 				return isActive; 
 			}
-            set { 
+			set { 
 				isActive = value;	
 			}
-        }
+		}
 
-        /// <summary>
-        /// 
-        /// </summary>
-        public override bool IsFullScreen {
-            get {
-                return base.IsFullScreen;
-            }
-        }
+		/// <summary>
+		/// 
+		/// </summary>
+		public override bool IsFullScreen {
+			get {
+				return base.IsFullScreen;
+			}
+		}
 
-        /// <summary>
-        ///     Saves the back buffer to disk.
-        /// </summary>
-        /// <param name="file"></param>
-        public override void SaveToFile(string file) {		
-            // get a reference to the back buffer surface
-            Surface surface = device.GetBackBuffer(0, 0, BackBufferType.Mono);
+		/// <summary>
+		///     Saves the back buffer to disk.
+		/// </summary>
+		/// <param name="file"></param>
+		public override void SaveToFile(string file) {		
+			// get a reference to the back buffer surface
+			// CMH 4/24/2004 - Start
+			Surface surface;
 
-            // save the surface to disk
-            SurfaceLoader.Save(file, ImageFileFormat.Jpg, surface);
-        }
+			if(isFullScreen) {
+				surface = device.GetBackBuffer(0, 0, BackBufferType.Mono);
+			}
+			else {
+				surface = swapChain.GetBackBuffer(0, BackBufferType.Mono);
+			}
+			// CMH - End
 
-        private void OnResetDevice(object sender, EventArgs e) {
-            Device resetDevice = (Device)sender;
+			// save the surface to disk
+			SurfaceLoader.Save(file, ImageFileFormat.Jpg, surface);
 
-            Console.WriteLine("Device has been reset!");
+			// dispose of the surface
+			surface.Dispose();
+		}
 
-            // Turn off culling, so we see the front and back of the triangle
-            resetDevice.RenderState.CullMode = Cull.None;
-            // Turn on the ZBuffer
-            resetDevice.RenderState.ZBufferEnable = true;
-            resetDevice.RenderState.Lighting = true;    //make sure lighting is enabled
-        }
+		private void OnResetDevice(object sender, EventArgs e) {
+			Device resetDevice = (Device)sender;
 
-        private void device_DeviceLost(object sender, EventArgs e) {
-            Console.WriteLine("Device has been lost!");
-        }
+			Console.WriteLine("Device has been reset!");
 
-        #endregion
-    }
+			// Turn off culling, so we see the front and back of the triangle
+			resetDevice.RenderState.CullMode = Cull.None;
+			// Turn on the ZBuffer
+			resetDevice.RenderState.ZBufferEnable = true;
+			resetDevice.RenderState.Lighting = true;    //make sure lighting is enabled
+		}
+
+		private void device_DeviceLost(object sender, EventArgs e) {
+			Console.WriteLine("Device has been lost!");
+		}
+
+		#endregion
+	}
 }
