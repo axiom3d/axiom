a05c65ba70f8948a744b7c4ad01382a763cc06fb
leedgitar <12863619+leedgitar@users.noreply.github.com>
2004-05-02 19:00:57 +0000

    - Autmoatically rebuild splines if keyframe data changed. - Added missing Remove* methods to AnimationTrack.

--- Axiom/Engine/Animating/KeyFrame.cs
+++ Axiom/Engine/Animating/KeyFrame.cs
@@ -29,47 +29,53 @@ using Axiom.MathLib;
 
 namespace Axiom.Animating {
     /// <summary>
-    /// A key frame in an animation sequence defined by an AnimationTrack.
+    ///		A key frame in an animation sequence defined by an AnimationTrack.
     /// </summary>
     ///	<remarks>
-    ///	This class can be used as a basis for all kinds of key frames. 
-    ///	The unifying principle is that multiple KeyFrames define an 
-    ///	animation sequence, with the exact state of the animation being an 
-    ///	interpolation between these key frames.
+    ///		This class can be used as a basis for all kinds of key frames. 
+    ///		The unifying principle is that multiple KeyFrames define an 
+    ///		animation sequence, with the exact state of the animation being an 
+    ///		interpolation between these key frames.
     /// </remarks>
-
     public class KeyFrame {
         #region Protected member variables
 
+		/// <summary>
+		///		Time of this keyframe.
+		/// </summary>
         protected float time;
+		/// <summary>
+		///		Translation at this keyframe.
+		/// </summary>
         protected Vector3 translate;
+		/// <summary>
+		///		Scale factor at this keyframe.
+		/// </summary>
         protected Vector3 scale;
+		/// <summary>
+		///		Rotation at this keyframe.
+		/// </summary>
         protected Quaternion rotation;
+		/// <summary>
+		///		Animation track that this key frame belongs to.
+		/// </summary>
+		protected AnimationTrack parentTrack;
 
         #endregion
 
         #region Constructors
 
         /// <summary>
-        /// Creates a new keyframe starting at time 0.
+        ///		Creates a new keyframe with the specified time.  
+        ///		Should really be created by <see cref="AnimationTrack.CreateKeyFrame"/> instead.
         /// </summary>
-        public KeyFrame() {
-            this.time = 0.0f;
-            this.translate = new Vector3();
-            this.scale = Vector3.UnitScale;
-            this.rotation = Quaternion.Identity;
-        }
-
-        /// <summary>
-        /// Creates a new keyframe with the specified time.  
-        /// Should really be created by AnimationTrack.CreateKeyFrame() instead.
-        /// </summary>
-        /// <param name="time"></param>
-        public KeyFrame(float time) {
+        /// <param name="parent">Animation track that this keyframe belongs to.</param>
+        /// <param name="time">Time at which this keyframe begins.</param>
+        public KeyFrame(AnimationTrack parent, float time) {
             this.time = time;
-            this.translate = new Vector3();
-            this.scale = Vector3.UnitScale;
-            this.rotation = Quaternion.Identity;
+            translate = new Vector3();
+            scale = Vector3.UnitScale;
+            rotation = Quaternion.Identity;
         }
 
         #endregion
@@ -77,42 +83,68 @@ namespace Axiom.Animating {
         #region Public properties
 
         /// <summary>
-        /// Sets the rotation applied by this keyframe.
-        ///	Use Quaternion methods to convert from angle/axis or Matrix3 if
-        ///	you don't like using Quaternions directly.
+        ///		Sets the rotation applied by this keyframe.
+        ///		Use Quaternion methods to convert from angle/axis or Matrix3 if
+        ///		you don't like using Quaternions directly.
         /// </summary>
         public Quaternion Rotation {
-            get { return this.rotation; }
-            set { this.rotation = value; }
+            get { 
+				return rotation; 
+			}
+            set { 
+				rotation = value;
+
+				if(parentTrack != null) {
+					parentTrack.OnKeyFrameDataChanged();
+				}
+			}
         }
         /// <summary>
-        /// Sets the scaling factor applied by this keyframe to the animable
-        ///	object at it's time index.
-        ///	beware of supplying zero values for any component of this
-        ///	vector, it will scale the object to zero dimensions.
+        ///		Sets the scaling factor applied by this keyframe to the animable
+        ///		object at it's time index.
+        ///		beware of supplying zero values for any component of this
+        ///		vector, it will scale the object to zero dimensions.
         /// </summary>
         public Vector3 Scale {
-            get { return this.scale; }
-            set { this.scale = value; }
+            get { 
+				return scale; 
+			}
+            set { 
+				scale = value;
+
+				if(parentTrack != null) {
+					parentTrack.OnKeyFrameDataChanged();
+				}
+			}
         }
 
         /// <summary>
-        /// Sets the translation associated with this keyframe. 
+        ///		Sets the translation associated with this keyframe. 
         /// </summary>
         /// <remarks>
-        ///	The translation factor affects how much the keyframe translates (moves) it's animable
-        ///	object at it's time index.
+        ///		The translation factor affects how much the keyframe translates (moves) it's animable
+        ///		object at it's time index.
         ///	</remarks>
         public Vector3 Translate {
-            get { return this.translate; }
-            set { this.translate = value; }
+            get { 
+				return translate; 
+			}
+            set { 
+				translate = value;
+
+				if(parentTrack != null) {
+					parentTrack.OnKeyFrameDataChanged();
+				}
+			}
         }
 
         /// <summary>
-        /// Gets the time of this keyframe in the animation sequence.
+        ///		Gets the time of this keyframe in the animation sequence.
         /// </summary>
         public float Time {
-            get { return this.time; }
+            get { 
+				return time; 
+			}
         }
 
         #endregion
