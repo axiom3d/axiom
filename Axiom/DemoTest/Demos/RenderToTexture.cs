bc285d6a87235383bfb284c2d8a9ac0f784d7d57
leedgitar <leedgitar@bb9bf1cf-960d-0410-9a3a-baeba60cf407>
2004-12-06 01:25:29 +0000

    - Fixed W Buffer check in D3D to only apply for 16 bit color. - Updated RenderToTexture demo!  Shows off texture projection and oblique depth projection for acheiving reflection without advanced hardware features.

--- Axiom/DemoTest/Demos/RenderToTexture.cs
+++ Axiom/DemoTest/Demos/RenderToTexture.cs
@@ -10,63 +10,103 @@ namespace Demos {
 	/// </summary>
 	public class RenderToTexture : TechDemo {
         
-        Camera rttCam;
-        SceneNode headNode;
-        Entity planeEnt;
+        Camera reflectCam;
         SceneNode planeNode;
+		MovablePlane plane;
+		Entity planeEntity;
 
         protected override void OnFrameStarted(object source, FrameEventArgs e) {
             base.OnFrameStarted (source, e);
 
-            rttCam.Position = camera.DerivedPosition;
-            rttCam.Direction = camera.DerivedDirection;
+			// make sure reflection camera is updated too
+            reflectCam.Orientation = camera.Orientation;
+			reflectCam.Position = camera.Position;
 
-            Quaternion q = rttCam.DerivedOrientation;
-            Vector3 vPos = rttCam.DerivedPosition;
-            Vector3 vDir = rttCam.DerivedDirection;
-
-            headNode.Orientation = q;
-            vDir.Normalize();
-
-            headNode.Position = vPos + vDir * -250.0f;
+			// rotate plane
+			planeNode.Yaw(30 * e.TimeSinceLastFrame, TransformSpace.Parent);
         }
 
         protected override void CreateScene() {
+			// set ambient light
+			scene.AmbientLight = new ColorEx(0.2f, 0.2f, 0.2f);
+
+			scene.SetSkyBox(true, "Skybox/Morning", 5000);
 
             // create a default point light
             Light light = scene.CreateLight("MainLight");
-            light.Position = new Vector3(20, 80, 50);
+			light.Type = LightType.Directional;
+			Vector3 dir = new Vector3(0.5f, -1, 0);
+			dir.Normalize();
+            light.Direction = dir;
+			light.Diffuse = new ColorEx(1.0f, 1.0f, 0.8f);
+			light.Specular = ColorEx.White;
 
-            planeEnt = scene.CreateEntity("Plane", PrefabEntity.Plane);
+			// create a plane
+			plane = new MovablePlane("ReflectPlane");
+			plane.D = 0;
+			plane.Normal = Vector3.UnitY;
+
+			// create another plane to create the mesh.  Ogre's MovablePlane uses multiple inheritance, bah!
+			Plane tmpPlane = new Plane();
+			tmpPlane.D = 0;
+			tmpPlane.Normal = Vector3.UnitY;
+
+			MeshManager.Instance.CreatePlane("ReflectionPlane", tmpPlane, 2000, 2000, 1, 1, true, 1, 1, 1, Vector3.UnitZ);
+            planeEntity = scene.CreateEntity("Plane", "ReflectionPlane");
+
+			// create an entity from a model
             Entity knot = scene.CreateEntity("Knot", "knot.mesh");
-            knot.MaterialName = "TextureFX/Knot";
+			knot.MaterialName = "TextureFX/Knot";
 
+			// create an entity from a model
             Entity head = scene.CreateEntity("Head", "ogrehead.mesh");
 
+			// attach the render to texture entity to the root of the scene
             SceneNode rootNode = scene.RootSceneNode;
+			planeNode = rootNode.CreateChildSceneNode();
 
-            headNode = rootNode.CreateChildSceneNode("Head");
-            headNode.AttachObject(head);
+			planeNode.AttachObject(planeEntity);
+			planeNode.AttachObject(plane);
+			planeNode.Translate(new Vector3(0, -10, 0));
 
-            planeNode = rootNode.CreateChildSceneNode();
-            planeNode.AttachObject(planeEnt);
+			// tilt it a little to make it interesting
+			planeNode.Roll(5);
 
-            rttCam = scene.CreateCamera("RttCam");
-            rootNode.AttachObject(rttCam);
-            rttCam.Position = new Vector3(0, 0, 200.0f);
-            rttCam.Direction = new Vector3(0.0f, 0.0f, 100.0f);
+			rootNode.CreateChildSceneNode("Head").AttachObject(head);
 
-            // create a render texture
-            RenderTexture rttTex = Root.Instance.RenderSystem.CreateRenderTexture("RttTex", 512, 512);
-            Viewport viewport = rttTex.AddViewport(rttCam, 0, 0, 1.0f, 1.0f, 0);
-            viewport.ClearEveryFrame = true;
-            viewport.OverlaysEnabled = false;
-            viewport.BackgroundColor = ColorEx.White;
+			// create a render texture
+			RenderTexture rttTex = Root.Instance.RenderSystem.CreateRenderTexture("RttTex", 512, 512);
+			reflectCam = scene.CreateCamera("ReflectCam");
+			reflectCam.Near = camera.Near;
+			reflectCam.Far = camera.Far;
+			reflectCam.AspectRatio = (float)window.GetViewport(0).ActualWidth / (float)window.GetViewport(0).ActualHeight;
+
+			Viewport viewport = rttTex.AddViewport(reflectCam);
+			viewport.ClearEveryFrame = true;
+			viewport.OverlaysEnabled = false;
+			viewport.BackgroundColor = ColorEx.Black;
          
             Material mat = scene.CreateMaterial("RttMat");
-            mat.GetTechnique(0).GetPass(0).CreateTextureUnitState("RttTex");
+			TextureUnitState t = mat.GetTechnique(0).GetPass(0).CreateTextureUnitState("RustedMetal.jpg");
+            t = mat.GetTechnique(0).GetPass(0).CreateTextureUnitState("RttTex");
 
-            planeEnt.MaterialName = "RttMat";
+			// blend with base texture
+			t.SetColorOperationEx(LayerBlendOperationEx.BlendManual, LayerBlendSource.Texture, LayerBlendSource.Current, 
+				ColorEx.White, ColorEx.White, 0.25f);
+
+			t.SetProjectiveTexturing(true, reflectCam);
+
+			// register events for viewport before/after update
+			rttTex.AfterUpdate += new RenderTargetUpdateEventHandler(rttTex_AfterUpdate);
+			rttTex.BeforeUpdate += new RenderTargetUpdateEventHandler(rttTex_BeforeUpdate);
+
+			// set up linked reflection
+			reflectCam.EnableReflection(plane);
+
+			// also clip
+			reflectCam.EnableCustomNearClipPlane(plane);
+
+            planeEntity.MaterialName = "RttMat";
 
             Entity clone = null;
 
@@ -76,9 +116,9 @@ namespace Demos {
 
                 // calculate a random position
                 Vector3 nodePosition = new Vector3();
-                nodePosition.x = MathUtil.SymmetricRandom() * 500.0f;
-                nodePosition.y = MathUtil.SymmetricRandom() * 500.0f;
-                nodePosition.z = MathUtil.SymmetricRandom() * 500.0f;
+                nodePosition.x = MathUtil.SymmetricRandom() * 750.0f;
+                nodePosition.y = MathUtil.SymmetricRandom() * 100.0f + 25;
+                nodePosition.z = MathUtil.SymmetricRandom() * 750.0f;
 
                 // set the new position
                 node.Position = nodePosition;
@@ -93,6 +133,27 @@ namespace Demos {
                 // add the cloned knot to the scene
                 node.AttachObject(clone);
             }
-        }
-    }
+
+			camera.Position = new Vector3(-50, 100, 500);
+			camera.LookAt(new Vector3(0, 0, 0));
+		}
+
+		/// <summary>
+		///		Hides the render target plane prior to the update.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void rttTex_BeforeUpdate(object sender, RenderTargetUpdateEventArgs e) {
+			planeEntity.IsVisible = false;
+		}
+
+		/// <summary>
+		///		Shows the render target plane again after the update.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void rttTex_AfterUpdate(object sender, RenderTargetUpdateEventArgs e) {
+			planeEntity.IsVisible = true;
+		}
+	}
 }
