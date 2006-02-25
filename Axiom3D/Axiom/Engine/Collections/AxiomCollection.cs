7deaf9fe420688cd086842042d1150d12a1c0396
c0d3g33k <c0d3g33k@bb9bf1cf-960d-0410-9a3a-baeba60cf407>
2006-02-25 14:18:48 +0000

    r107@realmforge.tigris.org (orig r119):  triplett | 2006-01-13 22:52:07 -0500  Removing extra endregions

--- Axiom3D/Axiom/Engine/Collections/AxiomCollection.cs
+++ Axiom3D/Axiom/Engine/Collections/AxiomCollection.cs
@@ -28,7 +28,6 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 using System;
 using System.Collections.Generic;
-#endregion Namespace declarations
 
 #endregion Namespace Declarations
 
