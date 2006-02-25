0d2921d4d471bfc74c40b2f97864431428d48740
Nikhil Mittal <16446505+c0d3G33k@users.noreply.github.com>
2006-02-25 14:41:27 +0000

    r135@realmforge.tigris.org (orig r150):  arilou | 2006-01-23 04:52:57 -0500  generic'ized some collections from Axiom.Graphics

--- Axiom3D/Axiom/Engine/Graphics/Collections.cs
+++ Axiom3D/Axiom/Engine/Graphics/Collections.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace Axiom
 {
@@ -13,35 +14,35 @@ namespace Axiom
     /// <summary>
     ///     Generics: List<TextureEffect>
     /// </summary>
-    public class TextureEffectList : ArrayList
+    public class TextureEffectList : List<TextureEffect>
     {
     }
 
     /// <summary>
     ///     Generics: List<RenderTarget>
     /// </summary>
-    public class RenderTargetList : ArrayList
+    public class RenderTargetList : List<RenderTarget>
     {
     }
 
     /// <summary>
     ///     Generics: List<RenderTexture>
     /// </summary>
-    public class RenderTextureList : ArrayList
+    public class RenderTextureList : List<RenderTexture>
     {
     }
 
     /// <summary>
     ///     Generics: List<Pass>
     /// </summary>
-    public class PassList : ArrayList
+    public class PassList : List<Pass>
     {
     }
 
     /// <summary>
     ///     Generics: List<Technique>
     /// </summary>
-    public class TechniqueList : ArrayList
+    public class TechniqueList : List<Technique>
     {
     }
 
@@ -55,14 +56,14 @@ namespace Axiom
     /// <summary>
     ///     Generics: List<AutoConstantEntry>
     /// </summary>
-    public class AutoConstantEntryList : ArrayList
+    public class AutoConstantEntryList : List<Axiom.GpuProgramParameters.AutoConstantEntry>
     {
     }
 
     /// <summary>
     ///     Generics: List<AutoConstantEntry>
     /// </summary>
-    public class FloatConstantEntryList : ArrayList
+    public class FloatConstantEntryList : List<Axiom.GpuProgramParameters.FloatConstantEntry>
     {
         public void Resize( int size )
         {
@@ -85,77 +86,78 @@ namespace Axiom
                 Add( new GpuProgramParameters.IntConstantEntry() );
             }
         }
+       
     }
 
     /// <summary>
     ///     Generics: List<IRenderable>
     /// </summary>
-    public class RenderableList : ArrayList
+    public class RenderableList : List<IRenderable>
     {
     }
 
     /// <summary>
     ///     Generics: List<EdgeData.Triangle>
     /// </summary>
-    public class TriangleList : ArrayList
+    public class TriangleList : List<EdgeData.Triangle>
     {
     }
 
     /// <summary>
     ///     Generics: List<EdgeData.Edge>
     /// </summary>
-    public class EdgeList : ArrayList
+    public class EdgeList : List<EdgeData.Edge>
     {
     }
 
     /// <summary>
     ///     Generics: List<EdgeGroup>
     /// </summary>
-    public class EdgeGroupList : ArrayList
+    public class EdgeGroupList : List<EdgeData.EdgeGroup>
     {
     }
 
     /// <summary>
     ///     Generics: List<VertexData>
     /// </summary>
-    public class VertexDataList : ArrayList
+    public class VertexDataList : List<VertexData>
     {
     }
 
     /// <summary>
     ///     Generics: List<IndexData>
     /// </summary>
-    public class IndexDataList : ArrayList
+    public class IndexDataList : List<IndexData>
     {
     }
 
     /// <summary>
     ///     Generics: List<ShadowRenderable>
     /// </summary>
-    public class ShadowRenderableList : ArrayList
+    public class ShadowRenderableList : List<ShadowRenderable>
     {
     }
 
     /// <summary>
     ///		Generics: List<RenderOperation>
     /// </summary>
-    public class OperationTypeList : ArrayList
+    public class OperationTypeList : List<OperationType>
     {
-        public void Add( OperationType type )
-        {
-            base.Add( type );
-        }
+        //public void Add( OperationType type )
+        //{
+        //    base.Add( type );
+        //}
 
-        public new OperationType this[int index]
-        {
-            get
-            {
-                return (OperationType)base[index];
-            }
-            set
-            {
-                base[index] = value;
-            }
-        }
+        //public new OperationType this[int index]
+        //{
+        //    get
+        //    {
+        //        return (OperationType)base[index];
+        //    }
+        //    set
+        //    {
+        //        base[index] = value;
+        //    }
+        //}
     }
 }
