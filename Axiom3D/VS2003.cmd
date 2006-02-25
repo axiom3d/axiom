17f8a1bed94355b99c8562fa95bddc58fbdd634d
c0d3g33k <c0d3g33k@bb9bf1cf-960d-0410-9a3a-baeba60cf407>
2006-02-25 14:14:51 +0000

    r102@realmforge.tigris.org (orig r114):  mcummings | 2006-01-12 09:16:08 -0500  - Fixed vs2003 prebuild command file

--- Axiom3D/VS2003.cmd
+++ Axiom3D/VS2003.cmd
@@ -1,3 +1,3 @@
 @rem Generates a solution (.sln) and a set of project files (.csproj) 
 @rem for Microsoft Visual Studio .NET 2002
-.\Tools\Prebuild3Prebuild /file prebuild.xml /target vs2003 /pause 
+.\Tools\Prebuild\Prebuild /file prebuild.xml /target vs2003 /pause 
