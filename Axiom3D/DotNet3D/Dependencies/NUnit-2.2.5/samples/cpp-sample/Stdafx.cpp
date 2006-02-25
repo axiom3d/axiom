b79a7b202c45a066cebc7d9e2a96be786100514b
c0d3g33k <c0d3g33k@bb9bf1cf-960d-0410-9a3a-baeba60cf407>
2006-02-25 14:11:18 +0000

    r97@realmforge.tigris.org (orig r109):  mcummings | 2006-01-11 12:03:32 -0500

---
+++ Axiom3D/DotNet3D/Dependencies/NUnit-2.2.5/samples/cpp-sample/Stdafx.cpp
@@ -0,0 +1,5 @@
+// stdafx.cpp : source file that includes just the standard includes
+// cpp-sample.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
