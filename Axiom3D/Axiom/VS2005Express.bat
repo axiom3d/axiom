f747ff0b8c9196d3ee3e3afb1b141de340aa1893
Nikhil Mittal <16446505+c0d3G33k@users.noreply.github.com>
2006-02-25 07:06:26 +0000

    r32@realmforge.tigris.org (orig r43):  mcummings | 2005-08-31 10:45:32 -0400  Removed some unused files and Added a Documentation Folder

--- Axiom3D/Axiom/VS2005Express.bat
+++
@@ -1,3 +0,0 @@
-@rem Generates a solution (.sln) and a set of project files (.csproj, .vbproj, etc.)
-@rem for Microsoft Visual Studio .NET 2005 Express
-../Prebuild /target vs2005express /allowedgroups Axiom|Cegui|Axiom+Cegui /pause
