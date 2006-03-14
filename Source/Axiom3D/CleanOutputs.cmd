76e34ac2bc9bef8d0f65378be7cebdd3d2e97a4d
Nikhil Mittal <16446505+c0d3G33k@users.noreply.github.com>
2006-03-14 16:35:55 +0000

    Interim updates to convenience scripts to work with new layout

--- Source/Axiom3D/CleanOutputs.cmd
+++ Source/Axiom3D/CleanOutputs.cmd
@@ -1,2 +1,2 @@
-.\Tools\Prebuild\Prebuild /removedir obj 
-.\Tools\Prebuild\Prebuild /removedir bin 
+..\..\Build_Tools\Prebuild\Prebuild /removedir obj 
+..\..\Build_Tools\Prebuild\Prebuild /removedir bin 
