0efa70140ed117bfa02566536c80adbf14f022b6
Michael Cummings <757221+borrillis@users.noreply.github.com>
2006-04-14 12:34:24 +0000

    - Quick Fix for 'Object must implement IConvertable'

--- Source/DotNet3D/Src/Math/Real.cs
+++ Source/DotNet3D/Src/Math/Real.cs
@@ -58,7 +58,7 @@ namespace DotNet3D.Math
     /// </remarks>
     [StructLayout( LayoutKind.Sequential )]
     [Serializable]
-    public struct Real : ISerializable, IComparable<Real>
+    public struct Real : ISerializable, IComparable<Real>, IConvertible
     {
         #region Fields
         /// <summary>
@@ -680,6 +680,95 @@ namespace DotNet3D.Math
         public int CompareTo( Real other )
         {
             return this._value.CompareTo( other._value );
+        }
+
+        #endregion
+
+        #region IConvertible Members
+
+        public TypeCode GetTypeCode()
+        {
+            throw new Exception( "The method or operation is not implemented." );
+        }
+
+        public bool ToBoolean( IFormatProvider provider )
+        {
+            throw new Exception( "The method or operation is not implemented." );
+        }
+
+        public byte ToByte( IFormatProvider provider )
+        {
+            throw new Exception( "The method or operation is not implemented." );
+        }
+
+        public char ToChar( IFormatProvider provider )
+        {
+            throw new Exception( "The method or operation is not implemented." );
+        }
+
+        public DateTime ToDateTime( IFormatProvider provider )
+        {
+            throw new Exception( "The method or operation is not implemented." );
+        }
+
+        public decimal ToDecimal( IFormatProvider provider )
+        {
+            throw new Exception( "The method or operation is not implemented." );
+        }
+
+        public double ToDouble( IFormatProvider provider )
+        {
+            throw new Exception( "The method or operation is not implemented." );
+        }
+
+        public short ToInt16( IFormatProvider provider )
+        {
+            throw new Exception( "The method or operation is not implemented." );
+        }
+
+        public int ToInt32( IFormatProvider provider )
+        {
+            throw new Exception( "The method or operation is not implemented." );
+        }
+
+        public long ToInt64( IFormatProvider provider )
+        {
+            throw new Exception( "The method or operation is not implemented." );
+        }
+
+        public sbyte ToSByte( IFormatProvider provider )
+        {
+            throw new Exception( "The method or operation is not implemented." );
+        }
+
+        public float ToSingle( IFormatProvider provider )
+        {
+            return (float)this;
+        }
+
+        public string ToString( IFormatProvider provider )
+        {
+            throw new Exception( "The method or operation is not implemented." );
+        }
+
+        public object ToType( Type conversionType, IFormatProvider provider )
+        {
+            throw new Exception( "The method or operation is not implemented." );
+        }
+
+        public ushort ToUInt16( IFormatProvider provider )
+        {
+            throw new Exception( "The method or operation is not implemented." );
+        }
+
+        public uint ToUInt32( IFormatProvider provider )
+        {
+            throw new Exception( "The method or operation is not implemented." );
+        }
+
+        public ulong ToUInt64( IFormatProvider provider )
+        {
+            throw new Exception( "The method or operation is not implemented." );
         }
 
         #endregion
