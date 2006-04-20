f9197a5785c93df5cd5a8ad1bae0a9ca52220f6a
Michael Cummings <757221+borrillis@users.noreply.github.com>
2006-04-20 17:20:07 +0000

    Removed file/folder

--- DISource/Axiom3D/SharpDevelop.cmd
+++
@@ -1,3 +0,0 @@
-@rem Generates a combine (.cmbx) and a set of project files (.prjx) 
-@rem for SharpDevelop 1.X (http://icsharpcode.net/OpenSource/SD/Default.aspx)
-..\..\Build_Tools\Prebuild\Prebuild /target sharpdev /pause
--- DISource/DotNet3D/nAnt.Smoke.All.cmd
+++
@@ -1,2 +0,0 @@
-..\..\Build_Tools\NAnt\bin\nAnt release smoke.all > NAnt.Smoke.All.Log
-notepad NAnt.Smoke.All.Log
