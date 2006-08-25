4c734d1ab4dbcffdb0e578ddaf590d0ba86725ee
borrillis <borrillis@bb9bf1cf-960d-0410-9a3a-baeba60cf407>
2006-08-25 11:05:33 +0000

    - Fixed D3DGpuProgram::LoadFromSource() was only checking for null string, not empty string.

--- Source/Axiom3D/RenderSystems/DirectX9/D3DGpuProgram.cs
+++ Source/Axiom3D/RenderSystems/DirectX9/D3DGpuProgram.cs
@@ -91,7 +91,7 @@ namespace Axiom.RenderSystems.DirectX9 {
             // load the shader from the source string
             GraphicsStream microcode = ShaderLoader.FromString(source, null, 0, out errors);
 
-            if(errors != null) {
+            if(errors != null && errors.Length != 0) {
                 string msg = string.Format("Error while compiling pixel shader '{0}':\n {1}", name, errors);
                 throw new AxiomException(msg);
             }
