b102be9a1cc857ec1f612cb1b7d088cf3033d7ca
Nikhil Mittal <16446505+c0d3G33k@users.noreply.github.com>
2006-10-11 14:29:27 +0000

    Adding some Linux build scripts

---
+++ Scripts/build_axiom3d_debug.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+mono ../BuildSupport/nAnt/bin/NAnt.exe -buildfile:../Source/Axiom3D/Axiom3D.build debug build.axiom3d
---
+++ Scripts/build_axiom3d_release.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+mono ../BuildSupport/nAnt/bin/NAnt.exe -buildfile:../Source/Axiom3D/Axiom3D.build release build.axiom3d
