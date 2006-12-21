a6fe2e1d8351914ae0a584afc31082feb030c522
borrillis <borrillis@bb9bf1cf-960d-0410-9a3a-baeba60cf407>
2006-12-21 22:06:45 +0000

    - More build system cleanup - Started moving AxiomDemos into new build system

---
+++ Projects/AxiomDemos/Scripts/IDE_VS2005.cmd
@@ -0,0 +1,3 @@
+@rem Generates a solution (.sln) and a set of project files (.csproj, .vbproj, etc.)
+@rem for Microsoft Visual Studio .NET 2005
+..\..\..\BuildSupport\Prebuild\Prebuild /file ..\Prebuild.xml /target vs2005 /pause
