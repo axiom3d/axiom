e8d31d1011c90cfc226174f34385bac41480686b
Michael Cummings <757221+borrillis@users.noreply.github.com>
2007-10-03 11:14:58 +0000

    - Fixed some Shutdown issues - Removed ConfigFile argument to Root constructor as it's never used.

--- Projects/Axiom/Source/Engine/Core/TextureManager.cs
+++ Projects/Axiom/Source/Engine/Core/TextureManager.cs
@@ -444,7 +444,7 @@ namespace Axiom.Core
 				// There are no unmanaged resources to release, but
 				// if we add them, they need to be released here.
 			}
-			isDisposed = true;
+			//isDisposed = true;
 
 			// If it is available, make the call to the
 			// base class's Dispose(Boolean) method
