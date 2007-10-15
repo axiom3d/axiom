9eb3218e4bce843cd13e8b6baf5a8e275e20d34a
borrillis <borrillis@bb9bf1cf-960d-0410-9a3a-baeba60cf407>
2007-10-15 02:34:08 +0000

    - Update for ParseReal to default to "en-US" for culture

--- Projects/Axiom/Source/Engine/Math/Utility.cs
+++ Projects/Axiom/Source/Engine/Math/Utility.cs
@@ -140,7 +140,7 @@ namespace Axiom.Math
 
 		public static Real ParseReal( string value )
 		{
-			return Real.Parse( value, System.Globalization.CultureInfo.CurrentCulture );
+			return Real.Parse( value, new CultureInfo( "en-US" ) );
 		}
 
         public static int Sign( Real number )
