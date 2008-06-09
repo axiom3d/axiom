d92ff585c9c7e1e4b056b7753da16399d492219e
Michael Cummings <757221+borrillis@users.noreply.github.com>
2008-06-09 19:13:35 +0000

    - Updated Tuple for proper implementation

--- Projects/Axiom/Source/Engine/Math/Tuple.cs
+++ Projects/Axiom/Source/Engine/Math/Tuple.cs
@@ -33,54 +33,150 @@ using System.Collections.Generic;
 
 namespace Axiom.Math
 {
-    /// <summary>
-    ///	Represents two related values
-    /// </summary>
-    public class Tuple< A, B >
-    {
-        public Tuple( A first, B second )
-        {
-            this.first = first;
-            this.second = second;
-        }
-        /// <summary></summary>
-        public A first;
-        /// <summary></summary>
-        public B second;
-    }
+	/// <summary>
+	///	Represents two related values
+	/// </summary>
+	public struct Tuple<A, B> : IEquatable<Tuple<A, B>>
+	{
+		#region Fields and Properties
 
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <typeparam name="A"></typeparam>
-    /// <typeparam name="B"></typeparam>
-    /// <typeparam name="C"></typeparam>
-    public class Tuple<A, B, C>
-    {
-        /// <summary></summary>
-        public A first;
-        /// <summary></summary>
-        public B second;
-        /// <summary></summary>
-        public C thrid;
-    }
+		/// <summary></summary>
+		public readonly A First;
+		/// <summary></summary>
+		public readonly B Second;
 
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <typeparam name="A"></typeparam>
-    /// <typeparam name="B"></typeparam>
-    /// <typeparam name="C"></typeparam>
-    /// <typeparam name="D"></typeparam>
-    public class Tuple<A, B, C, D>
-    {
-        /// <summary></summary>
-        public A first;
-        /// <summary></summary>
-        public B second;
-        /// <summary></summary>
-        public C thrid;
-        /// <summary></summary>
-        public D fourth;
-    }
+		#endregion Fields and Properties
+
+		#region Construction and Destruction
+		public Tuple( A first, B second )
+		{
+			this.First = first;
+			this.Second = second;
+		}
+		#endregion Construction and Destruction
+
+		#region IEquatable<Tuple<A,B>> Implementation
+
+		public bool Equals( Tuple<A, B> other )
+		{
+			return First.Equals( other.First ) &&
+				   Second.Equals( other.Second );
+
+		}
+
+		public bool Equals( object other )
+		{
+			if ( other is Tuple<A, B> )
+			{
+				return this.Equals( (Tuple<A, B>)other );
+			}
+			return false;
+		}
+
+		#endregion IEquatable<Tuple<A,B>> Implementation
+	}
+
+	/// <summary>
+	/// Represents three related values
+	/// </summary>
+	/// <typeparam name="A"></typeparam>
+	/// <typeparam name="B"></typeparam>
+	/// <typeparam name="C"></typeparam>
+	public struct Tuple<A, B, C> : IEquatable<Tuple<A, B, C>>
+	{
+		#region Fields and Properties
+
+		/// <summary></summary>
+		public readonly A First;
+		/// <summary></summary>
+		public readonly B Second;
+		/// <summary></summary>
+		public readonly C Third;
+
+		#endregion Fields and Properties
+
+		#region Construction and Destruction
+		public Tuple( A first, B second, C Third )
+		{
+			this.First = first;
+			this.Second = second;
+			this.Third = Third;
+		}
+		#endregion Construction and Destruction
+
+		#region IEquatable<Tuple<A,B,C>> Implementation
+
+		public bool Equals( Tuple<A, B, C> other )
+		{
+			return First.Equals( other.First )
+					&& Second.Equals( other.Second )
+					&& Third.Equals( other.Third );
+
+		}
+
+		public bool Equals( object other )
+		{
+			if ( other is Tuple<A, B, C> )
+			{
+				return this.Equals( (Tuple<A, B, C>)other );
+			}
+			return false;
+		}
+
+		#endregion IEquatable<Tuple<A,B,C>> Implementation
+	}
+
+	/// <summary>
+	/// Represents four related values
+	/// </summary>
+	/// <typeparam name="A"></typeparam>
+	/// <typeparam name="B"></typeparam>
+	/// <typeparam name="C"></typeparam>
+	/// <typeparam name="D"></typeparam>
+	public struct Tuple<A, B, C, D> : IEquatable<Tuple<A, B, C, D>>
+	{
+		#region Fields and Properties
+
+		/// <summary></summary>
+		public readonly A First;
+		/// <summary></summary>
+		public readonly B Second;
+		/// <summary></summary>
+		public readonly C Third;
+		/// <summary></summary>
+		public readonly D Fourth;
+
+		#endregion Fields and Properties
+
+		#region Construction and Destruction
+		public Tuple( A first, B second, C third, D fourth )
+		{
+			this.First = first;
+			this.Second = second;
+			this.Third = third;
+			this.Fourth = fourth;
+		}
+		#endregion Construction and Destruction
+
+		#region IEquatable<Tuple<A,B,C,D>> Implementation
+
+		public bool Equals( Tuple<A, B, C, D> other )
+		{
+			return First.Equals( other.First )
+					&& Second.Equals( other.Second )
+					&& Third.Equals( other.Third );
+
+		}
+
+		public bool Equals( object other )
+		{
+			if ( other is Tuple<A, B, C, D> )
+			{
+				return this.Equals( (Tuple<A, B, C, D>)other );
+			}
+			return false;
+		}
+
+		#endregion IEquatable<Tuple<A,B,C,D>> Implementation
+	}
 }
