c6acd9dcd62939d5d26a88fa6d0bf14ddda02ad1
borrillis <borrillis@bb9bf1cf-960d-0410-9a3a-baeba60cf407>
2009-08-24 21:41:28 +0000

    Ticket #35 - Fixed ConfigDialog for Commandline

--- Projects/AxiomDemos/Source/Browser/CommandLine/ConfigDialog.cs
+++ Projects/AxiomDemos/Source/Browser/CommandLine/ConfigDialog.cs
@@ -13,188 +13,234 @@ using System.Collections;
 
 namespace Axiom.Demos.Browser.CommandLine
 {
-	public enum DialogResult
-	{
-		Ok,
-		Cancel
-	}
+    public enum DialogResult
+    {
+        Ok,
+        Cancel
+    }
 
-	class ConfigDialog
-	{
-		private ConfigOption _renderSystems;
-		private RenderSystem _currentSystem;
-		private DialogResult _result;
-		private ConfigOption _currentOption;
-		private List<ConfigOption> _menuItems = new List<ConfigOption>();
+    /// <summary>
+    /// Command line configuration dialog
+    /// </summary>
+    class ConfigDialog
+    {
+        private ConfigOption _renderSystems;
+        private RenderSystem _currentSystem;
+        private DialogResult _result;
+        private ConfigOption _currentOption;
+        private ArrayList _menuItems = new ArrayList();
         private List<ConfigOption> _options = new List<ConfigOption>();
+        private int _numericInput;
 
-		public ConfigDialog()
-		{
-			_currentSystem = Root.Instance.RenderSystems.Values[0];
-			_renderSystems = new ConfigOption( "Render System", _currentSystem.Name, false );
-			foreach ( RenderSystem rs in Root.Instance.RenderSystems )
-			{
-				_renderSystems.PossibleValues.Add( _renderSystems.PossibleValues.Count, rs.ToString() );
-			}
-			BuildOptions();
-		}
+        public ConfigDialog()
+        {
+            Axiom.Core.Root root = Axiom.Core.Root.Instance;
+            _currentSystem = root.RenderSystems.Values[ 0 ];
+            _renderSystems = new ConfigOption("Render System", _currentSystem.Name, false);
 
-		private void BuildOptions()
-		{
-			int index = 1;
+            foreach ( RenderSystem rs in root.RenderSystems )
+            {
+                _renderSystems.PossibleValues.Add(_renderSystems.PossibleValues.Count, rs.ToString());
+            }
 
-			_options.Clear();
-			_options.Add( _renderSystems );
+            BuildOptions();
+        }
 
-			// Load Render Subsystem Options
-			foreach ( ConfigOption option in _currentSystem.ConfigOptions )
-			{
-				_options.Add( option );
-			}
-		}
+        private void BuildOptions()
+        {
+            _options.Clear();
+            _options.Add(_renderSystems);
 
-		private void BuildMenu()
-		{
-			_menuItems.Clear();
-			if ( _currentOption == null )
-				BuildMainMenu();
-			//else
-            //    BuildOptionMenu();
-		}
+            // Load Render Subsystem Options
+            foreach ( ConfigOption option in _currentSystem.ConfigOptions )
+            {
+                _options.Add(option);
+            }
+        }
 
-		private void BuildMainMenu()
-		{
-			for ( int index = 0; index < _options.Count; index++ )
-			{
-				_menuItems.Add( _options[ index ] );
-			}
-		}
+        private void BuildMenu()
+        {
+            _menuItems.Clear();
+            if ( _currentOption == null )
+                BuildMainMenu();
+            else
+                BuildOptionMenu();
+        }
 
-	        //private void BuildOptionMenu()
-        //{
-        //    foreach (string value in _currentOption.PossibleValues)
-        //    {
-        //        _menuItems.Add(value);
-        //    }
-        //}
+        private void BuildMainMenu()
+        {
+            for ( int index = 0; index < _options.Count; index++ )
+            {
+                _menuItems.Add(_options[ index ]);
+            }
+        }
 
-		private void DisplayOptions()
-		{
-			Console.Clear();
+        private void BuildOptionMenu()
+        {
+            for ( int index = 0; index < _currentOption.PossibleValues.Count; index++ )
+            {
+                _menuItems.Add(_currentOption.PossibleValues.Values[ index ].ToString());
+            }
+        }
 
-			Console.WriteLine( "Axiom Engine Configuration" );
-			Console.WriteLine( "==========================" );
+        private void DisplayOptions()
+        {
+            Console.Clear();
 
-			if ( _currentOption != null )
-			{
-				Console.WriteLine( "Available settings for {0}.\n", _currentOption.Name );
-			}
-			// Load Render Subsystem Options
-            int index = 0;
-            foreach ( object opt in _menuItems )
-			{
-                System.Console.WriteLine( "{0:D2}      | {1}", index++, opt.ToString() );
-			}
+            Console.WriteLine("Axiom Engine Configuration");
+            Console.WriteLine("==========================");
+
+            if ( _currentOption != null )
+            {
+                Console.WriteLine("Available settings for {0}.\n", _currentOption.Name);
+            }
+            // Load Render Subsystem Options
+            for ( int index = 0; index < _menuItems.Count; index++ )
+            {
+                System.Console.WriteLine("{0:D2}      | {1}", index, _menuItems[ index ].ToString());
+            }
 
             if ( _currentOption == null )
-			{
-				Console.WriteLine();
-				Console.WriteLine( "Enter  | Saves changes." );
-				Console.WriteLine( "ESC    | Exits." );
-			}
-			Console.Write( "\nSelect option : " );
-		}
-
-		private int GetInput()
-		{
-			int number = 0;
-			int keyCount = 2;
-
-			while ( keyCount > 0 )
-			{
-				ConsoleKeyInfo key = Console.ReadKey();
-
-				if ( key.Key == ConsoleKey.Escape )
-					return -1;
-				if ( key.Key == ConsoleKey.Enter )
-					return -2;
-
-				if ( key.Key.ToString().Substring( 1 ).Length == 1 && key.Key.ToString().Substring( 1 ).ToCharArray()[ 0 ] >= '0' && key.Key.ToString().Substring( 1 ).ToCharArray()[ 0 ] <= '9' )
-				{
-					number += Int32.Parse( key.Key.ToString().Substring( 1 ) ) * ( (int)System.Math.Pow( 10, keyCount - 1 ) );
-					keyCount--;
-				}
-			}
-			return number;
-		}
-
-		private bool ProcessKey( int key )
-		{
-			int index;
-
-			if ( _currentOption == null )
-			{
-				if ( key == -1 ) //ESCAPE
-				{
-					_result = DialogResult.Cancel;
-					return false;
-				}
-				if ( key == -2 )
-				{
-					Root.Instance.RenderSystem = _currentSystem;
-
-					//for ( index = 0; index < _options.Count; index++ )
-					//{
-					//    ConfigOption opt = (ConfigOption)_options[ index ];
-					//    _currentSystem.ConfigOptions[ opt.Name ] = opt;
-					//}
-
-					_result = DialogResult.Ok;
-					return false;
-				}
-
-				if ( key < _menuItems.Count )
-				{
-					_currentOption = _options.Find
-					(
-                        delegate(ConfigOption c)
-                        {
-                            return c == _menuItems[key];
-						}
-					);
-				}
+            {
+                Console.WriteLine();
+                Console.WriteLine("Enter  | Save changes");
+                Console.WriteLine("ESC    | Exit");
             }
-			else
-			{
-				_currentOption.Value = _currentOption.PossibleValues[key].ToString();
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("ESC    | Back to main");
+            }
 
-				if ( _currentOption.Name == "Render System" ) // About to change Renderers
-				{
-					_renderSystems = _currentOption;
-					_currentSystem = Root.Instance.RenderSystems[key.ToString()];
-					BuildOptions();
-					_currentOption = null;
+            Console.Write("\nSelect option : ");
+        }
 
-					return true;
-				}
+        private int GetInput()
+        {
+            _numericInput = -1;
+            string stringInput = String.Empty;
+            int keyCount = 2;
 
-				_currentOption = null;
-			}
-			return true;
-		}
+            while ( keyCount > 0 )
+            {
+                ConsoleKeyInfo key = Console.ReadKey();
 
-		public DialogResult ShowDialog()
-		{
-			bool _continue = false;
-			do
-			{
-				BuildMenu();
-				DisplayOptions();
-				int value = GetInput();
-				_continue = ProcessKey( value );
-			} while ( _continue );
-			Console.Clear();
-			return _result;
-		}
-	}
+                if ( key.Key == ConsoleKey.Escape )
+                    return -1;
+
+                if ( key.Key == ConsoleKey.Enter )
+                {
+                    if ( stringInput.Length > 0 )
+                        _numericInput = Int32.Parse(stringInput);
+
+                    return -2;
+                }
+
+                if ( key.KeyChar >= '0' && key.KeyChar <= '9' )
+                {
+                    stringInput += key.KeyChar.ToString();
+                    keyCount--;
+                }
+                else if ( key.Key == ConsoleKey.Backspace )
+                {
+                    if ( stringInput.Length > 0 )
+                    {
+                        stringInput = stringInput.Substring(0, stringInput.Length - 1);
+                        keyCount++;
+                        Console.Write(" \b"); // clear last char
+                    }
+                    else
+                        Console.Write(" "); // back to position
+                }
+                else
+                {
+                    Console.Write("\b \b"); // clear invalid char
+                }
+            }
+            _numericInput = Int32.Parse(stringInput);
+
+            return -2; // force ENTER
+        }
+
+        private bool ProcessKey( int key )
+        {
+
+            if ( key == -1 ) //ESCAPE
+            {
+                if ( _currentOption == null )
+                {
+                    // ESC at main menu
+                    _result = DialogResult.Cancel;
+                    return false;
+                }
+                else
+                {
+                    // go back to main menu
+                    _currentOption = null;
+                    return true;
+                }
+            }
+
+            if ( key == -2 ) //ENTER
+            {
+                if ( _currentOption == null )
+                {
+                    // at main menu
+                    if ( _numericInput == -1 )
+                    {
+                        Axiom.Core.Root.Instance.RenderSystem = _currentSystem;
+
+                        _result = DialogResult.Ok;
+                        return false;
+                    }
+                    else if ( _numericInput >= 0 && _numericInput < _menuItems.Count )
+                    {
+                        _currentOption = (ConfigOption)_menuItems[ _numericInput ];
+                        return true;
+                    }
+
+                    return true;
+                }
+                else if ( _numericInput >= 0 && _numericInput < _currentOption.PossibleValues.Count )
+                {
+                    // at options menu and having an entered number
+                    _currentOption.Value = _currentOption.PossibleValues.Values[ _numericInput ].ToString();
+
+                    if ( _currentOption.Name == "Render System" ) // About to change Renderers
+                    {
+                        _renderSystems = _currentOption;
+                        _currentSystem = Axiom.Core.Root.Instance.RenderSystems.Values[ _numericInput ];
+
+                        BuildOptions();
+
+                        _currentOption = null;
+
+                        return true;
+                    }
+
+                    _currentOption = null;
+                }
+
+                return true;
+            }
+
+            return true;
+        }
+
+        public DialogResult ShowDialog()
+        {
+            bool _continue = false;
+            do
+            {
+                BuildMenu();
+                DisplayOptions();
+                int value = GetInput();
+                _continue = ProcessKey(value);
+            } while ( _continue );
+
+            Console.Clear();
+
+            return _result;
+        }
+    }
 }
