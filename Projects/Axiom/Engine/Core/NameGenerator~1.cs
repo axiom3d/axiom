6f00876f42368b7b4fc577c505672a94aecc0382
Michael Cummings <757221+borrillis@users.noreply.github.com>
2009-09-14 09:31:27 +0000

    - Added generic NameGenerator for use in custom Dictionaries, etc

---
+++ Projects/Axiom/Engine/Core/NameGenerator~1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Axiom.Core
+{
+    public class NameGenerator<T>
+    {
+        private static long _nextId;
+        private static string _baseName;
+
+        public long NextIdentifier
+        {
+            get
+            {
+                return _nextId;
+            }
+            set
+            {
+                _nextId = value;
+            }
+        }
+
+        public NameGenerator()
+            : this( typeof( T ).Name )
+        {
+        }
+
+        public NameGenerator(string baseName)
+        {
+            if ( _baseName != null )
+                _baseName = baseName;
+        }
+
+        public string GetNextUniqueName()
+        {
+            return GetNextUniqueName( String.Empty );
+        }
+
+        public string GetNextUniqueName( string prefix )
+        {
+            return String.Format( "{0}{1}{2}", prefix, _baseName, _nextId++ );
+        }
+    }
+}
