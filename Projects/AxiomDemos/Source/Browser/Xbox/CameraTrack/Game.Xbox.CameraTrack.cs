90ab43c058027d4406b14f489251dc6aafe891c0
borrillis <borrillis@bb9bf1cf-960d-0410-9a3a-baeba60cf407>
2009-12-02 22:05:24 +0000

    - Updated Xbox CameraTrack Demo * Added Xbox BezierPatch Demo

--- Projects/AxiomDemos/Source/Browser/Xbox/CameraTrack/Game.Xbox.CameraTrack.cs
+++ Projects/AxiomDemos/Source/Browser/Xbox/CameraTrack/Game.Xbox.CameraTrack.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq; 
 using System.Text;
 
+using Axiom.Core;
+
 namespace Axiom.Demos.Browser.Xna
 {
     partial class Game
@@ -10,6 +12,19 @@ namespace Axiom.Demos.Browser.Xna
         partial void _setDefaultNextGame()
         {
             this.nextGame = "CameraTrack";
+        }
+        
+        /// <summary>
+        ///		Loads default resource configuration if one exists.
+        /// </summary>
+        partial void _setupResources()
+        {
+            ResourceGroupManager.Instance.AddResourceLocation( titleLocation + "Content\\Fonts", "Folder" );
+            ResourceGroupManager.Instance.AddResourceLocation( titleLocation + "Content\\Materials\\Programs", "Folder" );
+            ResourceGroupManager.Instance.AddResourceLocation( titleLocation + "Content\\Materials\\Scripts", "Folder" );
+            ResourceGroupManager.Instance.AddResourceLocation( titleLocation + "Content\\Materials\\Textures", "Folder" );
+            ResourceGroupManager.Instance.AddResourceLocation( titleLocation + "Content\\Models", "Folder" );
+            ResourceGroupManager.Instance.AddResourceLocation( titleLocation + "Content\\Overlays", "Folder" );
         }
 
     }
