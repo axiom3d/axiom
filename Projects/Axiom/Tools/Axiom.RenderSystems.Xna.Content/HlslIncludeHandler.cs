91320b3027814386020f6889b8e43913f861ac11
Michael Cummings <757221+borrillis@users.noreply.github.com>
2009-12-03 20:54:04 +0000

    - HlslProcessor     + Added HlslIncludeHandler     + Added Preprocessor Defines     * Fixed call to ShaderCompiler to include CompilerOptions.PackMatrixRowMajor

---
+++ Projects/Axiom/Tools/Axiom.RenderSystems.Xna.Content/HlslIncludeHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content.Pipeline;
+using System.IO;
+
+namespace Axiom.RenderSystems.Xna.Content
+{
+    class HlslIncludeHandler : CompilerIncludeHandler
+    {
+        public override Stream Open( CompilerIncludeHandlerType includeType, string filename )
+        {
+            return File.Open( filename, FileMode.Open );
+        }
+    }
+}
