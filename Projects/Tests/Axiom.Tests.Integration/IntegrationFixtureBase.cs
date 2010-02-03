0cb460d2a5397b26aaa37fc786cd7aee6b9b200e
Michael Cummings <757221+borrillis@users.noreply.github.com>
2010-02-03 16:32:49 +0000

    - Added Integration Test project   These will be for full blown tests that can't ( or shouldn't ) be done in isolation.   isloated tests should go in Axiom.Tests.Unit.

---
+++ Projects/Tests/Axiom.Tests.Integration/IntegrationFixtureBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Gallio.Framework;
+using MbUnit.Framework;
+using MbUnit.Framework.ContractVerifiers;
+
+namespace Axiom.Tests.Integration
+{
+    [TestFixture]
+    public class IntegrationFixtureBase
+    {
+        [Test]
+        public void Test()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+    }
+}
