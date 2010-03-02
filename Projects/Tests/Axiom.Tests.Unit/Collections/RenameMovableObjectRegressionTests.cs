57da23d0ce8129ea0dc3221cf8f7ff6baa4dc5f1
Michael Cummings <757221+borrillis@users.noreply.github.com>
2010-03-02 15:11:45 +0000

    Ticket #169 - Added Tests for renaming MovableObjects

---
+++ Projects/Tests/Axiom.Tests.Unit/Collections/RenameMovableObjectRegressionTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Axiom.Core;
+using Axiom.Core.Collections;
+
+using Gallio.Framework;
+using MbUnit.Framework;
+using MbUnit.Framework.ContractVerifiers;
+
+using TypeMock;
+using TypeMock.ArrangeActAssert;
+
+namespace Axiom.UnitTests.Collections
+{
+    [TestFixture]
+    public class RenameMovableObjectRegressionTests
+    {
+        private SceneManager sceneManager;
+        private Entity entity;
+
+        /// <summary>
+        /// Sets up each test.
+        /// </summary>
+        [SetUp]
+        public void SetUp()
+        {
+            this.sceneManager = Isolate.Fake.Instance<SceneManager>();
+            Isolate.WhenCalled( () => this.sceneManager.RootSceneNode ).WillReturn( new SceneNode( this.sceneManager, "Root" ) );
+            this.entity = Isolate.Fake.Instance<Entity>();
+        }
+
+
+        [Test]
+        public void SceneNode_AddRenameEntity_ShouldNotThrowException()
+        {
+
+            sceneManager.RootSceneNode.AttachObject( entity );
+            entity.Name = "newName";
+            Assert.IsTrue( sceneManager.RootSceneNode.GetObject( entity.Name ) != null );
+
+            sceneManager.RootSceneNode.DetachAllObjects();
+        }
+
+        [Test]
+        public void SceneManager_AddRenameEntity_ShouldNotThrowException()
+        {
+
+            sceneManager.RootSceneNode.AttachObject( entity );
+            entity.Name = "newName";
+            Assert.IsTrue( sceneManager.GetMovableObject( entity.Name, "Entity" ) != null );
+
+            sceneManager.RootSceneNode.DetachAllObjects();
+        }
+    }
+}
