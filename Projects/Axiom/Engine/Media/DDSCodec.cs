5a42929b8dbaa7d15e3656b22091075c070c1b78
borrillis <borrillis@bb9bf1cf-960d-0410-9a3a-baeba60cf407>
2010-05-26 13:37:24 +0000

    - XBOX360 changes   - ScriptableObject uses Type.FindInterfaces which isn't available in the XNA Framework on the 360, used foreach ( in Type.GetInterfaces() ) ...   - CompositorManager used SortedDictionary<K,T> which isn't available in the XNA FRamework on the 360, replaced with SortedList<K,T>   - AtomAbstractNode used float.TryParse, implemented using explicit try {} catch () {}   - Updated Axiom.360 project file to include missing files - DDSCodec.cs updated to actually compile.

--- Projects/Axiom/Engine/Media/DDSCodec.cs
+++ Projects/Axiom/Engine/Media/DDSCodec.cs
@@ -34,6 +34,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 #region Namespace Declarations
 
 using System;
+using System.IO;
 
 using Axiom.Core;
 using Axiom.Math;
@@ -115,7 +116,7 @@ namespace Axiom.Media
         /// <summary>
         /// Finalizer
         /// </summary>
-        public virtual ~DDSCodec()
+        ~DDSCodec()
         {
         }
 
