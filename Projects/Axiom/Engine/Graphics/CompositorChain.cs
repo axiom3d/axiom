e141b6f170a255efe960dd7834b8442df2678bcc
borrillis <borrillis@bb9bf1cf-960d-0410-9a3a-baeba60cf407>
2010-08-23 18:57:27 +0000

    - Updated Copyright notices

--- Projects/Axiom/Engine/Graphics/CompositorChain.cs
+++ Projects/Axiom/Engine/Graphics/CompositorChain.cs
@@ -2,7 +2,7 @@
 
 /*
 Axiom Graphics Engine Library
-Copyright (C) 2003-2006  Axiom Project Team
+Copyright (C) 2003-2010 Axiom Project Team
 
 The overall design, and a majority of the core engine and rendering code
 contained within this library is a derivative of the open source Object Oriented
--- Projects/Axiom/Engine/Utilities/Exceptions.cs
+++ Projects/Axiom/Engine/Utilities/Exceptions.cs
@@ -1,7 +1,7 @@
 #region LGPL License
 /*
 Axiom Graphics Engine Library
-Copyright (C) 2003-2006 Axiom Project Team
+Copyright (C) 2003-2010 Axiom Project Team
 
 The overall design, and a majority of the core engine and rendering code 
 contained within this library is a derivative of the open source Object Oriented 
@@ -26,7 +26,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 #region SVN Version Information
 // <file>
-//     <license see="http://axiomengine.sf.net/wiki/index.php/license.txt"/>
+//     <license see="http://axiom3d.net/wiki/index.php/license.txt"/>
 //     <id value="$Id: AxiomException.cs 939 2006-12-06 01:39:38Z borrillis $"/>
 // </file>
 #endregion SVN Version Information
