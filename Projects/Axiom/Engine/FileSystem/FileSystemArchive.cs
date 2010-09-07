91d27dd29de3922415db38143eb3f76397222316
borrillis <borrillis@bb9bf1cf-960d-0410-9a3a-baeba60cf407>
2010-09-07 14:22:14 +0000

    - Added MonoDroid project + Solution - Updated Condition Defines to get Core compiling on MonoDroid. - Fixed StreamSerializer for lack of Debug.Write

--- Projects/Axiom/Engine/FileSystem/FileSystemArchive.cs
+++ Projects/Axiom/Engine/FileSystem/FileSystemArchive.cs
@@ -44,321 +44,321 @@ using Axiom.Core;
 
 namespace Axiom.FileSystem
 {
-    /// <summary>
-    /// Specialization of the Archive class to allow reading of files from filesystem folders / directories.
-    /// </summary>
-    /// <ogre name="FileSystemArchive">
-    ///     <file name="OgreFileSystem.h"   revision="1.6.2.1" lastUpdated="5/18/2006" lastUpdatedBy="Borrillis" />
-    ///     <file name="OgreFileSystem.cpp" revision="1.8" lastUpdated="5/18/2006" lastUpdatedBy="Borrillis" />
-    /// </ogre>
-    public class FileSystemArchive : Archive
-    {
-        #region Fields and Properties
+	/// <summary>
+	/// Specialization of the Archive class to allow reading of files from filesystem folders / directories.
+	/// </summary>
+	/// <ogre name="FileSystemArchive">
+	///     <file name="OgreFileSystem.h"   revision="1.6.2.1" lastUpdated="5/18/2006" lastUpdatedBy="Borrillis" />
+	///     <file name="OgreFileSystem.cpp" revision="1.8" lastUpdated="5/18/2006" lastUpdatedBy="Borrillis" />
+	/// </ogre>
+	public class FileSystemArchive : Archive
+	{
+		#region Fields and Properties
 
-        /// <summary>Base path; actually the same as Name, but for clarity </summary>
-        private string _basePath;
+		/// <summary>Base path; actually the same as Name, but for clarity </summary>
+		private string _basePath;
 
-        /// <summary>Directory stack of previous directories </summary>
-        private Stack<string> _directoryStack = new Stack<string>();
+		/// <summary>Directory stack of previous directories </summary>
+		private Stack<string> _directoryStack = new Stack<string>();
 
-        /// <summary>
-        /// Is this archive capable of being monitored for additions, changes and deletions
-        /// </summary>
-        public override bool IsMonitorable
-        {
-            get
-            {
-                return true;
-            }
-        }
+		/// <summary>
+		/// Is this archive capable of being monitored for additions, changes and deletions
+		/// </summary>
+		public override bool IsMonitorable
+		{
+			get
+			{
+				return true;
+			}
+		}
 
-        #endregion Fields and Properties
+		#endregion Fields and Properties
 
-        #region Utility Methods
+		#region Utility Methods
 
-        protected delegate void Action();
+		protected delegate void Action();
 
-        protected void SafeDirectoryChange( string directory, Action action )
-        {
-            if ( Directory.Exists( directory ) )
-            {
-                // Check we can change to it
-                pushDirectory( _basePath );
+		protected void SafeDirectoryChange( string directory, Action action )
+		{
+			if ( Directory.Exists( directory ) )
+			{
+				// Check we can change to it
+				pushDirectory( _basePath );
 
-                try
-                {
-                    action();
-                }
-                catch ( Exception ex )
-                {
-                    LogManager.Instance.Write( LogManager.BuildExceptionString( ex ) );
-                }
-                finally
-                {
-                    // return to previous
-                    popDirectory();
-                }
-            }
-        }
+				try
+				{
+					action();
+				}
+				catch ( Exception ex )
+				{
+					LogManager.Instance.Write( LogManager.BuildExceptionString( ex ) );
+				}
+				finally
+				{
+					// return to previous
+					popDirectory();
+				}
+			}
+		}
 
-        /// <overloads>
-        /// <summary>
-        /// Utility method to retrieve all files in a directory matching pattern.
-        /// </summary>
-        /// <param name="pattern">File pattern</param>
-        /// <param name="recursive">Whether to cascade down directories</param>
-        /// <param name="simpleList">Populated if retrieving a simple list</param>
-        /// <param name="detailList">Populated if retrieving a detailed list</param>
-        /// </overloads>
-        protected void findFiles( string pattern, bool recursive, List<string> simpleList, FileInfoList detailList )
-        {
-            findFiles( pattern, recursive, simpleList, detailList, "" );
-        }
-        /// <param name="currentDir">The current directory relative to the base of the archive, for file naming</param>
-        protected void findFiles( string pattern, bool recursive, List<string> simpleList, FileInfoList detailList, string currentDir )
-        {
-            if ( pattern == "" )
-                pattern = "*";
-            if ( currentDir == "" ) currentDir = _basePath;
+		/// <overloads>
+		/// <summary>
+		/// Utility method to retrieve all files in a directory matching pattern.
+		/// </summary>
+		/// <param name="pattern">File pattern</param>
+		/// <param name="recursive">Whether to cascade down directories</param>
+		/// <param name="simpleList">Populated if retrieving a simple list</param>
+		/// <param name="detailList">Populated if retrieving a detailed list</param>
+		/// </overloads>
+		protected void findFiles( string pattern, bool recursive, List<string> simpleList, FileInfoList detailList )
+		{
+			findFiles( pattern, recursive, simpleList, detailList, "" );
+		}
+		/// <param name="currentDir">The current directory relative to the base of the archive, for file naming</param>
+		protected void findFiles( string pattern, bool recursive, List<string> simpleList, FileInfoList detailList, string currentDir )
+		{
+			if ( pattern == "" )
+				pattern = "*";
+			if ( currentDir == "" ) currentDir = _basePath;
 
-#if !( XBOX || XBOX360 )
-            SearchOption so;
-            if ( recursive )
-            {
-                so = SearchOption.AllDirectories;
-            }
-            else
-            {
-                so = SearchOption.TopDirectoryOnly;
-            }
+#if !( XBOX || XBOX360 || ANDROID )
+			SearchOption so;
+			if ( recursive )
+			{
+				so = SearchOption.AllDirectories;
+			}
+			else
+			{
+				so = SearchOption.TopDirectoryOnly;
+			}
 
-            foreach ( string file in Directory.GetFiles( currentDir, pattern, so ) )
+			foreach ( string file in Directory.GetFiles( currentDir, pattern, so ) )
 #else
-            foreach( string file in Directory.GetFiles( currentDir, pattern ) )
+			foreach( string file in Directory.GetFiles( currentDir, pattern ) )
 #endif
-            {
-                System.IO.FileInfo fi = new System.IO.FileInfo( file );
-                if ( simpleList != null )
-                {
-                    simpleList.Add( fi.Name );
-                }
-                if ( detailList != null )
-                {
-                    FileInfo fileInfo;
-                    fileInfo.Archive = this;
-                    fileInfo.Filename = fi.FullName;
-                    fileInfo.Basename = fi.FullName.Substring( currentDir.Length );
-                    fileInfo.Path = currentDir;
-                    fileInfo.CompressedSize = fi.Length;
-                    fileInfo.UncompressedSize = fi.Length;
-                    detailList.Add( fileInfo );
-                }
-            }
-        }
+			{
+				System.IO.FileInfo fi = new System.IO.FileInfo( file );
+				if ( simpleList != null )
+				{
+					simpleList.Add( fi.Name );
+				}
+				if ( detailList != null )
+				{
+					FileInfo fileInfo;
+					fileInfo.Archive = this;
+					fileInfo.Filename = fi.FullName;
+					fileInfo.Basename = fi.FullName.Substring( currentDir.Length );
+					fileInfo.Path = currentDir;
+					fileInfo.CompressedSize = fi.Length;
+					fileInfo.UncompressedSize = fi.Length;
+					detailList.Add( fileInfo );
+				}
+			}
+		}
 
-        /// <summary>Utility method to change the current directory </summary>
-        protected void changeDirectory( string dir )
-        {
-            Directory.SetCurrentDirectory( dir );
-        }
+		/// <summary>Utility method to change the current directory </summary>
+		protected void changeDirectory( string dir )
+		{
+			Directory.SetCurrentDirectory( dir );
+		}
 
-        /// <summary>Utility method to change directory and push the current directory onto a stack </summary>
-        void pushDirectory( string dir )
-        {
-            // get current directory and push it onto the stack
+		/// <summary>Utility method to change directory and push the current directory onto a stack </summary>
+		void pushDirectory( string dir )
+		{
+			// get current directory and push it onto the stack
 #if !( XBOX || XBOX360 )
-            string cwd = Directory.GetCurrentDirectory();
-            _directoryStack.Push( cwd );
+			string cwd = Directory.GetCurrentDirectory();
+			_directoryStack.Push( cwd );
 #endif
-            changeDirectory( dir );
-        }
+			changeDirectory( dir );
+		}
 
-        /// <summary>Utility method to pop a previous directory off the stack and change to it </summary>
-        void popDirectory()
-        {
-            if ( _directoryStack.Count == 0 )
-            {
+		/// <summary>Utility method to pop a previous directory off the stack and change to it </summary>
+		void popDirectory()
+		{
+			if ( _directoryStack.Count == 0 )
+			{
 #if !( XBOX || XBOX360 )
-                throw new AxiomException( "No directories left in the stack." );
+				throw new AxiomException( "No directories left in the stack." );
 #else
-                return;
+				return;
 #endif
-            }
-            string cwd = _directoryStack.Pop();
-            changeDirectory( cwd );
-        }
+			}
+			string cwd = _directoryStack.Pop();
+			changeDirectory( cwd );
+		}
 
-        #endregion Utility Methods
+		#endregion Utility Methods
 
-        #region Constructors and Destructors
+		#region Constructors and Destructors
 
-        public FileSystemArchive( string name, string archType )
-            : base( name, archType )
-        {
-        }
+		public FileSystemArchive( string name, string archType )
+			: base( name, archType )
+		{
+		}
 
-        ~FileSystemArchive()
-        {
-            Unload();
-        }
+		~FileSystemArchive()
+		{
+			Unload();
+		}
 
-        #endregion Constructors and Destructors
+		#endregion Constructors and Destructors
 
-        #region Archive Implementation
+		#region Archive Implementation
 
-        public override bool IsCaseSensitive
-        {
-            get
-            {
-                return !PlatformManager.IsWindowsOS;
-            }
-        }
+		public override bool IsCaseSensitive
+		{
+			get
+			{
+				return !PlatformManager.IsWindowsOS;
+			}
+		}
 
-        public override void Load()
-        {
-            _basePath = Path.GetFullPath( Name ) + Path.DirectorySeparatorChar;
-            IsReadOnly = false;
+		public override void Load()
+		{
+			_basePath = Path.GetFullPath( Name ) + Path.DirectorySeparatorChar;
+			IsReadOnly = false;
 
-            SafeDirectoryChange( _basePath, () =>
-                                            {
-                                                try
-                                                {
-                                                
-#if !( XBOX || XBOX360 )
-                                                    File.Create( _basePath + @"__testWrite.Axiom", 1, FileOptions.DeleteOnClose );
+			SafeDirectoryChange( _basePath, () =>
+											{
+												try
+												{
+
+#if !( XBOX || XBOX360 || ANDROID )
+													File.Create( _basePath + @"__testWrite.Axiom", 1, FileOptions.DeleteOnClose );
 #else
-                                                    File.Create(_basePath + @"__testWrite.Axiom", 1 );
+													File.Create(_basePath + @"__testWrite.Axiom", 1 );
 #endif
-                                                }
-                                                catch( Exception ex )
-                                                {
-                                                    IsReadOnly = true;
-                                                }
-                                            } );
+												}
+												catch( Exception ex )
+												{
+													IsReadOnly = true;
+												}
+											} );
 
-        }
+		}
 
-        public override Stream Create( string filename, bool overwrite )
-        {
-            if ( IsReadOnly )
-            {
-                throw new AxiomException( "Cannot create a file in a read-only archive." );
-            }
+		public override Stream Create( string filename, bool overwrite )
+		{
+			if ( IsReadOnly )
+			{
+				throw new AxiomException( "Cannot create a file in a read-only archive." );
+			}
 
-            Stream stream = null;
-            string fullPath = _basePath + Path.DirectorySeparatorChar + filename;
-            bool exists = File.Exists( fullPath );
-            if ( !exists || overwrite )
-            {
-                try
-                {
-#if !( XBOX || XBOX360 )
-                    stream = File.Create( fullPath, 1, FileOptions.RandomAccess );
+			Stream stream = null;
+			string fullPath = _basePath + Path.DirectorySeparatorChar + filename;
+			bool exists = File.Exists( fullPath );
+			if ( !exists || overwrite )
+			{
+				try
+				{
+#if !( XBOX || XBOX360 || ANDROID )
+					stream = File.Create( fullPath, 1, FileOptions.RandomAccess );
 #else
-                    stream = File.Create( fullPath, 1 );
+					stream = File.Create( fullPath, 1 );
 #endif
-                }
-                catch ( Exception ex )
-                {
-                    throw new AxiomException( "Failed to open file : " + filename, ex );
-                }
-            }
-            else
-            {
-                stream = Open( fullPath, false );
-            }
+				}
+				catch ( Exception ex )
+				{
+					throw new AxiomException( "Failed to open file : " + filename, ex );
+				}
+			}
+			else
+			{
+				stream = Open( fullPath, false );
+			}
 
-            return stream;
-        }
+			return stream;
+		}
 
-        public override void Unload()
-        {
-            // Nothing to do here.
-        }
+		public override void Unload()
+		{
+			// Nothing to do here.
+		}
 
-        public override System.IO.Stream Open( string filename, bool readOnly )
-        {
-            Stream strm = null;
+		public override System.IO.Stream Open( string filename, bool readOnly )
+		{
+			Stream strm = null;
 
-            SafeDirectoryChange( _basePath, () =>
-                                            {
-                                                if ( File.Exists( _basePath + filename ) )
-                                                {
-                                                    System.IO.FileInfo fi = new System.IO.FileInfo( _basePath + filename );
-                                                    strm = (Stream) fi.Open( FileMode.Open, readOnly ? FileAccess.Read : FileAccess.ReadWrite );
-                                                }
-                                            } );
+			SafeDirectoryChange( _basePath, () =>
+											{
+												if ( File.Exists( _basePath + filename ) )
+												{
+													System.IO.FileInfo fi = new System.IO.FileInfo( _basePath + filename );
+													strm = (Stream) fi.Open( FileMode.Open, readOnly ? FileAccess.Read : FileAccess.ReadWrite );
+												}
+											} );
 
-            return strm;
-        }
+			return strm;
+		}
 
-        public override List<string> List( bool recursive )
-        {
-            return Find( "*", recursive );
-        }
+		public override List<string> List( bool recursive )
+		{
+			return Find( "*", recursive );
+		}
 
-        public override FileInfoList ListFileInfo( bool recursive )
-        {
-            return FindFileInfo( "*", recursive );
-        }
+		public override FileInfoList ListFileInfo( bool recursive )
+		{
+			return FindFileInfo( "*", recursive );
+		}
 
-        public override List<string> Find( string pattern, bool recursive )
-        {
-            List<string> ret = new List<string>();
+		public override List<string> Find( string pattern, bool recursive )
+		{
+			List<string> ret = new List<string>();
 
-            SafeDirectoryChange( _basePath, () => findFiles( pattern, recursive, ret, null ) );
+			SafeDirectoryChange( _basePath, () => findFiles( pattern, recursive, ret, null ) );
 
-            return ret;
-        }
+			return ret;
+		}
 
-        public override FileInfoList FindFileInfo( string pattern, bool recursive )
-        {
-            FileInfoList ret = new FileInfoList();
+		public override FileInfoList FindFileInfo( string pattern, bool recursive )
+		{
+			FileInfoList ret = new FileInfoList();
 
-            SafeDirectoryChange( _basePath, () => findFiles( pattern, recursive, null, ret ) );
+			SafeDirectoryChange( _basePath, () => findFiles( pattern, recursive, null, ret ) );
 
-            return ret;
-        }
+			return ret;
+		}
 
-        public override bool Exists( string fileName )
-        {
-            return File.Exists( _basePath + fileName );
-        }
+		public override bool Exists( string fileName )
+		{
+			return File.Exists( _basePath + fileName );
+		}
 
-        #endregion Archive Implementation
-    }
+		#endregion Archive Implementation
+	}
 
-    /// <summary>
-    /// Specialization of IArchiveFactory for FileSystem files.
-    /// </summary>
-    /// <ogre name="FileSystemArchiveFactory">
-    ///     <file name="OgreFileSystem.h"   revision="1.6.2.1" lastUpdated="5/18/2006" lastUpdatedBy="Borrillis" />
-    ///     <file name="OgreFileSystem.cpp" revision="1.8" lastUpdated="5/18/2006" lastUpdatedBy="Borrillis" />
-    /// </ogre>
-    public class FileSystemArchiveFactory : ArchiveFactory
-    {
-        private const string _type = "Folder";
+	/// <summary>
+	/// Specialization of IArchiveFactory for FileSystem files.
+	/// </summary>
+	/// <ogre name="FileSystemArchiveFactory">
+	///     <file name="OgreFileSystem.h"   revision="1.6.2.1" lastUpdated="5/18/2006" lastUpdatedBy="Borrillis" />
+	///     <file name="OgreFileSystem.cpp" revision="1.8" lastUpdated="5/18/2006" lastUpdatedBy="Borrillis" />
+	/// </ogre>
+	public class FileSystemArchiveFactory : ArchiveFactory
+	{
+		private const string _type = "Folder";
 
-        #region ArchiveFactory Implementation
+		#region ArchiveFactory Implementation
 
-        public override string Type
-        {
-            get
-            {
-                return _type;
-            }
-        }
+		public override string Type
+		{
+			get
+			{
+				return _type;
+			}
+		}
 
-        public override Archive CreateInstance( string name )
-        {
-            return new FileSystemArchive( name, _type );
-        }
+		public override Archive CreateInstance( string name )
+		{
+			return new FileSystemArchive( name, _type );
+		}
 
-        public override void DestroyInstance( ref Archive obj )
-        {
-            obj.Dispose();
-        }
+		public override void DestroyInstance( ref Archive obj )
+		{
+			obj.Dispose();
+		}
 
-        #endregion ArchiveFactory Implementation
-    };
+		#endregion ArchiveFactory Implementation
+	};
 }
