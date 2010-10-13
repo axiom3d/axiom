cca159cc57c528c0ceb7658ccaaa25257c2e2fab
bostich1983 <bostich1983@bb9bf1cf-960d-0410-9a3a-baeba60cf407>
2010-10-13 21:06:57 +0000

    -added particle fx. Axiom misses 3 parsing propertys point_sprites on point_size 2 point_size_attenuation on

---
+++ Projects/Samples/ParticleFX/Plugin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Axiom.Samples.ParticleFX
+{
+    public class Plugin : SamplePlugin
+    {
+        private ParticleFXSample sample;
+        public override void Initialize()
+        {
+            sample = new ParticleFXSample();
+            Name = sample.Metadata[ "Title" ] + " Sample";
+            AddSample( sample );
+        }
+    }
+}
