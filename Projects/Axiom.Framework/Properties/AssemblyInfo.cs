5e0e4917c3072e4629acc6af3f101a9bac816cbf
borrillis <borrillis@bb9bf1cf-960d-0410-9a3a-baeba60cf407>
2010-10-13 13:17:06 +0000

    - missed a couple of changes from last commit

---
+++ Projects/Axiom.Framework/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Axiom Framework")]
+[assembly: AssemblyDescription("Framework class for Axiom 3D Engine")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Axiom Project Team")]
+[assembly: AssemblyProduct("Axiom Framework")]
+[assembly: AssemblyCopyright("Copyright ©  2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9EBD47AD-48FE-4D4E-B611-2C7A53917DE0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
