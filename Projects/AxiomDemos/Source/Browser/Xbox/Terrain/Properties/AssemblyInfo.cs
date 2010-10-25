f8d97886ef11e87f63bbd31f1b0c5bfee2f7bd82
Michael Cummings <757221+borrillis@users.noreply.github.com>
2010-10-25 17:08:30 +0000

    Added Xbox Project for SkeletalAnimation, untested, but compiles.

--- Projects/AxiomDemos/Source/Browser/Xbox/Terrain/Properties/AssemblyInfo.cs
+++ Projects/AxiomDemos/Source/Browser/Xbox/Terrain/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle( "Axiom Camera Track Demo" )]
+[assembly: AssemblyTitle( "Axiom Terrain Demo" )]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 // It's also used by XNA
