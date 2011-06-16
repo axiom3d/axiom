f90be06aa21095bde691a48d3ff8af6c2ee556f6
wolfgangsteff <wolfgangsteff@bb9bf1cf-960d-0410-9a3a-baeba60cf407>
2011-06-16 21:33:37 +0000

    added TextureMipmapBias to TextureUnitState

--- Projects/Axiom/Engine/Graphics/TextureUnitState.cs
+++ Projects/Axiom/Engine/Graphics/TextureUnitState.cs
@@ -893,6 +893,9 @@ namespace Axiom.Graphics
 			set;
 		}
 
+        [OgreVersion(1, 7)]
+        public float TextureMipmapBias { get; set; }
+
 		#endregion Fields and Properties
 
 		#region Constructors
