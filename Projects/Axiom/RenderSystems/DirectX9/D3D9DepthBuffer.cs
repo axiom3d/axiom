e50868f0ec16a0fe61f332af1fd2368361f7750c
Wolfgang Steffens <42481+WolfgangSt@users.noreply.github.com>
2011-06-17 00:23:40 +0000

    finalized port of D3D9DepthBuffer. Completley at v1.7 now

--- Projects/Axiom/RenderSystems/DirectX9/D3D9DepthBuffer.cs
+++ Projects/Axiom/RenderSystems/DirectX9/D3D9DepthBuffer.cs
@@ -2,24 +2,50 @@
 using Axiom.Graphics;
 using SlimDX.Direct3D9;
 
+// ReSharper disable InconsistentNaming
+
 namespace Axiom.RenderSystems.DirectX9
 {
+    [OgreVersion(1, 7, 2790)]
     public class D3D9DepthBuffer : DepthBuffer
     {
+        #region depthBuffer
+
         [OgreVersion(1, 7, 2790)]
+
         protected Surface depthBuffer;
+
+        #endregion
+
+        #region creator
 
         [OgreVersion(1, 7, 2790)]
         protected Device creator;
 
+        #endregion
+
+        #region multiSampleQuality
+
         [OgreVersion(1, 7, 2790)]
         protected int multiSampleQuality;
+
+        #endregion
+
+        #region d3dFormat
 
         [OgreVersion(1, 7, 2790)]
         protected Format d3dFormat;
 
+        #endregion
+
+        #region renderSystem
+
         [OgreVersion(1, 7, 2790)]
         protected D3DRenderSystem renderSystem;
+
+        #endregion
+
+        #region DeviceCreator
 
         [OgreVersion(1, 7, 2790)]
         public Device DeviceCreator
@@ -30,6 +56,10 @@ namespace Axiom.RenderSystems.DirectX9
             }
         }
 
+        #endregion
+
+        #region DepthBufferSurface
+
         [OgreVersion(1, 7, 2790)]
         public Surface DepthBufferSurface
         {
@@ -38,6 +68,10 @@ namespace Axiom.RenderSystems.DirectX9
                 return depthBuffer;
             }
         }
+
+        #endregion
+
+        #region Constructor
 
         public D3D9DepthBuffer( PoolId poolId, D3DRenderSystem renderSystem,
             Device creator, Surface depthBufferSurf,
@@ -49,7 +83,7 @@ namespace Axiom.RenderSystems.DirectX9
             depthBuffer = depthBufferSurf;
             this.creator = creator;
             this.multiSampleQuality = multiSampleQuality;
-			d3dFormat = fmt;
+            d3dFormat = fmt;
             this.renderSystem = renderSystem;
 
             switch (fmt)
@@ -69,5 +103,52 @@ namespace Axiom.RenderSystems.DirectX9
                     break;
             }
         }
+
+        #endregion
+
+        #region Dispose
+
+        protected override void dispose(bool disposeManagedResources)
+        {
+            if (!manual)
+                depthBuffer.Dispose();
+            depthBuffer = null;
+
+            creator = null;
+
+            base.dispose(disposeManagedResources);
+        }
+
+        #endregion
+
+        #region IsCompatible
+
+        [OgreVersion(1, 7, 2790)]
+        public override bool IsCompatible(RenderTarget renderTarget)
+        {
+
+            var pBack = (Surface[])renderTarget[ "DDBACKBUFFER" ];
+            if ( pBack[ 0 ] == null )
+                return false;
+
+            var srfDesc = pBack[ 0 ].Description;
+
+            //RenderSystem will determine if bitdepths match (i.e. 32 bit RT don't like 16 bit Depth)
+            //This is the same function used to create them. Note results are usually cached so this should
+            //be quick
+            var fmt = renderSystem.GetDepthStencilFormatFor( srfDesc.Format );
+            var activeDevice = D3DRenderSystem.ActiveD3D9Device;
+
+            return creator == activeDevice &&
+                   fmt == d3dFormat &&
+                   fsaa == (int)srfDesc.MultisampleType &&
+                   multiSampleQuality == srfDesc.MultisampleQuality &&
+                   Width >= renderTarget.Width &&
+                   Height >= renderTarget.Height;
+        }
+
+        #endregion
     }
 }
+
+// ReSharper restore InconsistentNaming
