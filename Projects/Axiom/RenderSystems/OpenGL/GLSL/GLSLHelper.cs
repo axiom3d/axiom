16d2a52f45551a2ccffcf4d67d596cee5bedc8ef
Wolfgang Steffens <42481+WolfgangSt@users.noreply.github.com>
2011-06-17 00:41:08 +0000

    bugfix that caused crashing within native code. it actually seems to be an opentk error this workaround fixes.

--- Projects/Axiom/RenderSystems/OpenGL/GLSL/GLSLHelper.cs
+++ Projects/Axiom/RenderSystems/OpenGL/GLSL/GLSLHelper.cs
@@ -115,6 +115,7 @@ namespace Axiom.RenderSystems.OpenGL.GLSL
 				{
 					int charsWritten = 0;
 
+				    logMessage.EnsureCapacity( infologLength + 1 );
 					Gl.glGetInfoLogARB( handle, infologLength, out charsWritten, logMessage );
 					if ( charsWritten > 0 )
 					{
