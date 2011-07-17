ed6d4c8a23abc127604026fdc1ab264bd9e6c743
borrillis <borrillis@bb9bf1cf-960d-0410-9a3a-baeba60cf407>
2011-07-17 16:12:34 +0000

    Wolfgang Steffens <WolfgangSteff@gmail.com> missing files from patches

---
+++ Projects/Axiom/Engine/Utilities/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Axiom.Utilities
+{
+    public static class Extensions
+    {
+        public static string AsString(this Stream s)
+        {
+            using (var r = new StreamReader(s))
+                return r.ReadToEnd();
+        }
+    }
+}
