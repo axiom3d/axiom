cf7aa9403de7e281a41cbae44f99680783e0d428
borrillis <borrillis@bb9bf1cf-960d-0410-9a3a-baeba60cf407>
2011-08-18 15:55:43 +0000

    updated LinearResampler.Byte to use safe code ticket:259

--- Projects/Axiom/Engine/Media/LinearResampler.Byte.cs
+++ Projects/Axiom/Engine/Media/LinearResampler.Byte.cs
@@ -79,59 +79,60 @@ namespace Axiom.Media
 					return;
 				}
 
-				unsafe
+				// srcdata stays at beginning of slice, pdst is a moving pointer
+				byte[] srcdata = new byte[ src.ConsecutiveSize ];
+				byte[] dstdata = new byte[ dst.ConsecutiveSize ];
+				int pdst = 0;
+
+				src.Data.GetData( srcdata );
+
+				// sx_48,sy_48 represent current position in source
+				// using 16/48-bit fixed precision, incremented by steps
+				UInt64 stepx = (UInt64)( ( src.Width << 48 ) / dst.Width );
+				UInt64 stepy = (UInt64)( ( src.Height << 48 ) / dst.Height );
+
+				// bottom 28 bits of temp are 16/12 bit fixed precision, used to
+				// adjust a source coordinate backwards by half a pixel so that the
+				// integer bits represent the first sample (eg, sx1) and the
+				// fractional bits are the blend weight of the second sample
+				uint temp;
+
+				UInt64 sy_48 = ( stepy >> 1 ) - 1;
+				for ( uint y = (uint)dst.Top; y < dst.Bottom; y++, sy_48 += stepy )
 				{
-					// srcdata stays at beginning of slice, pdst is a moving pointer
-					byte* srcdata = (byte*)src.Data;
-					byte* pdst = (byte*)dst.Data;
+					temp = (uint)( sy_48 >> 36 );
+					temp = ( temp > 0x800 ) ? temp - 0x800 : 0;
+					uint syf = temp & 0xFFF;
+					uint sy1 = temp >> 12;
+					uint sy2 = (uint)System.Math.Min( sy1 + 1, src.Bottom - src.Top - 1 );
+					uint syoff1 = (uint)( sy1 * src.RowPitch );
+					uint syoff2 = (uint)( sy2 * src.RowPitch );
 
-					// sx_48,sy_48 represent current position in source
-					// using 16/48-bit fixed precision, incremented by steps
-					UInt64 stepx = (UInt64)( ( src.Width << 48 ) / dst.Width );
-					UInt64 stepy = (UInt64)( ( src.Height << 48 ) / dst.Height );
-
-					// bottom 28 bits of temp are 16/12 bit fixed precision, used to
-					// adjust a source coordinate backwards by half a pixel so that the
-					// integer bits represent the first sample (eg, sx1) and the
-					// fractional bits are the blend weight of the second sample
-					uint temp;
-
-					UInt64 sy_48 = ( stepy >> 1 ) - 1;
-					for ( uint y = (uint)dst.Top; y < dst.Bottom; y++, sy_48 += stepy )
+					UInt64 sx_48 = ( stepx >> 1 ) - 1;
+					for ( uint x = (uint)dst.Left; x < dst.Right; x++, sx_48 += stepx )
 					{
-						temp = (uint)( sy_48 >> 36 );
+						temp = (uint)( sx_48 >> 36 );
 						temp = ( temp > 0x800 ) ? temp - 0x800 : 0;
-						uint syf = temp & 0xFFF;
-						uint sy1 = temp >> 12;
-						uint sy2 = (uint)System.Math.Min( sy1 + 1, src.Bottom - src.Top - 1 );
-						uint syoff1 = (uint)( sy1 * src.RowPitch );
-						uint syoff2 = (uint)( sy2 * src.RowPitch );
+						uint sxf = temp & 0xFFF;
+						uint sx1 = temp >> 12;
+						uint sx2 = (uint)System.Math.Min( sx1 + 1, src.Right - src.Left - 1 );
 
-						UInt64 sx_48 = ( stepx >> 1 ) - 1;
-						for ( uint x = (uint)dst.Left; x < dst.Right; x++, sx_48 += stepx )
+						uint sxfsyf = sxf * syf;
+						for ( uint k = 0; k < this._channels; k++ )
 						{
-							temp = (uint)( sx_48 >> 36 );
-							temp = ( temp > 0x800 ) ? temp - 0x800 : 0;
-							uint sxf = temp & 0xFFF;
-							uint sx1 = temp >> 12;
-							uint sx2 = (uint)System.Math.Min( sx1 + 1, src.Right - src.Left - 1 );
-
-							uint sxfsyf = sxf * syf;
-							for ( uint k = 0; k < this._channels; k++ )
-							{
-								uint accum = (uint)(
-													   srcdata[ ( sx1 + syoff1 ) * this._channels + k ] * (char)( 0x1000000 - ( sxf << 12 ) - ( syf << 12 ) + sxfsyf ) +
-													   srcdata[ ( sx2 + syoff1 ) * this._channels + k ] * (char)( ( sxf << 12 ) - sxfsyf ) +
-													   srcdata[ ( sx1 + syoff2 ) * this._channels + k ] * (char)( ( syf << 12 ) - sxfsyf ) +
-													   srcdata[ ( sx2 + syoff2 ) * this._channels + k ] * (char)sxfsyf );
-								// accum is computed using 8/24-bit fixed-point math
-								// (maximum is 0xFF000000; rounding will not cause overflow)
-								*pdst++ = (byte)( ( accum + 0x800000 ) >> 24 );
-							}
+							uint accum = (uint)(
+												   srcdata[ ( sx1 + syoff1 ) * this._channels + k ] * (char)( 0x1000000 - ( sxf << 12 ) - ( syf << 12 ) + sxfsyf ) +
+												   srcdata[ ( sx2 + syoff1 ) * this._channels + k ] * (char)( ( sxf << 12 ) - sxfsyf ) +
+												   srcdata[ ( sx1 + syoff2 ) * this._channels + k ] * (char)( ( syf << 12 ) - sxfsyf ) +
+												   srcdata[ ( sx2 + syoff2 ) * this._channels + k ] * (char)sxfsyf );
+							// accum is computed using 8/24-bit fixed-point math
+							// (maximum is 0xFF000000; rounding will not cause overflow)
+							dstdata[ pdst++ ] = (byte)( ( accum + 0x800000 ) >> 24 );
 						}
-						pdst += this._channels * dst.RowSkip;
 					}
+					pdst += this._channels * dst.RowSkip;
 				}
+				dst.Data.SetData( dstdata );
 			}
 		}
 	}
