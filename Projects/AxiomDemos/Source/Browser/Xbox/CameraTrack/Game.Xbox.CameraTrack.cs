705e3914cc6449a2a9d1a63800f2452d2f1c2ef9
borrillis <borrillis@bb9bf1cf-960d-0410-9a3a-baeba60cf407>
2011-10-07 21:31:50 +0000

    Updated CameraTrack WP7 Demo to use TitleContainerArchive

--- Projects/AxiomDemos/Source/Browser/Xbox/CameraTrack/Game.Xbox.CameraTrack.cs
+++ Projects/AxiomDemos/Source/Browser/Xbox/CameraTrack/Game.Xbox.CameraTrack.cs
@@ -2,46 +2,33 @@ using Axiom.Core;
 
 namespace Axiom.Demos.Browser.Xna
 {
-    partial class Game
-    {
-        partial void _setDefaultNextGame()
-        {
-            this.nextGame = "CameraTrack";
-        }
+	partial class Game
+	{
+		partial void _setDefaultNextGame()
+		{
+			this.nextGame = "CameraTrack";
+		}
 
-        /// <summary>
-        ///		Loads default resource configuration if one exists.
-        /// </summary>
-        partial void _setupResources()
-        {
+		/// <summary>
+		///		Loads default resource configuration if one exists.
+		/// </summary>
+		partial void _setupResources()
+		{
 #if !WINDOWS_PHONE
-            ResourceGroupManager.Instance.AddResourceLocation( "Content\\Fonts", "Folder" );
-            ResourceGroupManager.Instance.AddResourceLocation( "Content\\Materials\\Programs", "Folder" );
-            ResourceGroupManager.Instance.AddResourceLocation( "Content\\Materials\\Scripts", "Folder" );
-            ResourceGroupManager.Instance.AddResourceLocation( "Content\\Materials\\Textures", "Folder" );
-            ResourceGroupManager.Instance.AddResourceLocation( "Content\\Models", "Folder" );
-            ResourceGroupManager.Instance.AddResourceLocation( "Content\\Overlays", "Folder" );
+			ResourceGroupManager.Instance.AddResourceLocation( "Content\\Fonts", "Folder" );
+			ResourceGroupManager.Instance.AddResourceLocation( "Content\\Materials\\Programs", "Folder" );
+			ResourceGroupManager.Instance.AddResourceLocation( "Content\\Materials\\Scripts", "Folder" );
+			ResourceGroupManager.Instance.AddResourceLocation( "Content\\Materials\\Textures", "Folder" );
+			ResourceGroupManager.Instance.AddResourceLocation( "Content\\Models", "Folder" );
+			ResourceGroupManager.Instance.AddResourceLocation( "Content\\Overlays", "Folder" );
 #else
-            string grp = ResourceGroupManager.DefaultResourceGroupName;
-
-            ResourceGroupManager.Instance.DeclareResource( "Fonts/BlueHighway.png", "Texture", grp );
-            ResourceGroupManager.Instance.DeclareResource( "Materials/Textures/clouds.jpg", "Texture", grp );
-            ResourceGroupManager.Instance.DeclareResource( "Materials/Textures/AxiomLogo.png", "Texture", grp );
-            ResourceGroupManager.Instance.DeclareResource( "Materials/Textures/Border.png", "Texture", grp );
-            ResourceGroupManager.Instance.DeclareResource( "Materials/Textures/Border_Break.png", "Texture", grp );
-            ResourceGroupManager.Instance.DeclareResource( "Materials/Textures/Border_Center.png", "Texture", grp );
-            ResourceGroupManager.Instance.DeclareResource( "Materials/Textures/dirt01.jpg", "Texture", grp );
-            ResourceGroupManager.Instance.DeclareResource( "Materials/Textures/GreenSky.jpg", "Texture", grp );
-            ResourceGroupManager.Instance.DeclareResource( "Materials/Textures/RustySteel.jpg", "Texture", grp );
-            ResourceGroupManager.Instance.DeclareResource( "Models/ogrehead.mesh", "Mesh", grp );
-
-            _parseScript( "Content/Materials/Scripts/Skys.material", grp, "Material" );
-            _parseScript( "Content/Materials/Scripts/Core.material", grp, "Material" );
-            _parseScript( "Content/Materials/Scripts/RustySteel.material", grp, "Material" );
-            _parseScript( "Content/Fonts/BlueHighway.fontdef", grp, "Font" );
-            _parseScript( "Content/Overlays/DebugPanel.overlay", grp, "Overlay" );
-            ResourceGroupManager.Instance.InitializeResourceGroup( grp );
+			ResourceGroupManager.Instance.AddResourceLocation( "Content\\Fonts", "TitleContainer" );
+			ResourceGroupManager.Instance.AddResourceLocation( "Content\\Materials\\Programs", "TitleContainer" );
+			ResourceGroupManager.Instance.AddResourceLocation( "Content\\Materials\\Scripts", "TitleContainer" );
+			ResourceGroupManager.Instance.AddResourceLocation( "Content\\Materials\\Textures", "TitleContainer" );
+			ResourceGroupManager.Instance.AddResourceLocation( "Content\\Models", "TitleContainer" );
+			ResourceGroupManager.Instance.AddResourceLocation( "Content\\Overlays", "TitleContainer" );
 #endif
-        }
-    }
+		}
+	}
 }
