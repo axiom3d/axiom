809b9334a1218e57372412c68d9d8a17002693a2
Michael Cummings <757221+borrillis@users.noreply.github.com>
2012-01-18 08:03:41 +0000

    Added AssemblyVersion task + targets

---
+++ tools/build/AxiomBuildTasks/AssemblyInfoFile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Axiom.Build.Tasks
+{
+	class AssemblyInfoFile
+	{
+		public string Template
+		{
+			get;
+			set;
+		}
+
+		public string OutputFile
+		{
+			get;
+			set;
+		}
+	}
+}
