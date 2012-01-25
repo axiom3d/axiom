5e5dfc377d6c2ac57ac0f11b907486dc6a889577
romeoxbm <romeoxbm@bb9bf1cf-960d-0410-9a3a-baeba60cf407>
2012-01-25 20:21:08 +0000

    Updated comments for D3DTextureStageDesc and added license notice to its file;

--- Projects/Axiom/RenderSystems/DirectX9/D3DTextureStageDesc.cs
+++ Projects/Axiom/RenderSystems/DirectX9/D3DTextureStageDesc.cs
@@ -1,25 +1,76 @@
-﻿using Axiom.Core;
+﻿#region MIT/X11 License
+//Copyright © 2003-2012 Axiom 3D Rendering Engine Project
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+//
+//The above copyright notice and this permission notice shall be included in
+//all copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//THE SOFTWARE.
+#endregion License
+
+#region SVN Version Information
+// <file>
+//     <license see="http://axiom3d.net/wiki/index.php/license.txt"/>
+//     <id value="$Id$"/>
+// </file>
+#endregion SVN Version Information
+
+#region Namespace Declarations
+
+using Axiom.Core;
 using Axiom.Graphics;
 using SlimDX.Direct3D9;
+
+#endregion Namespace Declarations
 
 namespace Axiom.RenderSystems.DirectX9
 {
     /// <summary>
-    ///		Structure holding texture unit settings for every stage
+    ///	Structure holding texture unit settings for every stage
     /// </summary>
+    [OgreVersion( 1, 7, 2 )]
     internal struct D3DTextureStageDesc
     {
-        /// the type of the texture
+        /// <summary>
+        /// The type of the texture
+        /// </summary>
         public D3DTextureType texType;
-        /// which texCoordIndex to use
+
+        /// <summary>
+        /// Which texCoordIndex to use
+        /// </summary>
         public int coordIndex;
-        /// type of auto tex. calc. used
+
+        /// <summary>
+        /// Type of auto tex. calc. used
+        /// </summary>
         public TexCoordCalcMethod autoTexCoordType;
+
+        /// <summary>
         /// Frustum, used if the above is projection
+        /// </summary>
         public Frustum frustum;
-        /// texture
+
+        /// <summary>
+        /// Texture
+        /// </summary>
         public BaseTexture tex;
-        /// vertex texture
+        
+        /// <summary>
+        /// Vertex texture
+        /// </summary>
         public BaseTexture vertexTex;
-    }
+    };
 }
