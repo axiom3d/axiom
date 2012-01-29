7345c23497860c70f02e8bb7175b2716ad0b8bff
usulthemouse <usulthemouse@bb9bf1cf-960d-0410-9a3a-baeba60cf407>
2012-01-29 02:48:16 +0000

    Nuget Upload, moved to NuGet Package directory and started making the command to run package.

---
+++ Projects/Axiom/NuGet Package/buildnuspec.bat
@@ -0,0 +1,16 @@
+@echo off
+SET PATH=..\..\..\..\..\Tools\Nuget\;%PATH%
+mkdir ..\..\dist\nuspec
+
+nuget pack Axiom.nuspec -OutputDirectory ..\..\dist\nuspec
+nuget pack Axiom.Platforms.Win32.nuspec -OutputDirectory ..\..\dist\nuspec
+nuget pack Axiom.RenderSystems.DirectX9.nuspec -OutputDirectory ..\..\dist\nuspec
+nuget pack Axiom.RenderSystems.Xna.nuspec -OutputDirectory ..\..\dist\nuspec
+nuget pack Axiom.Plugins.FreeImageCodecs.nuspec -OutputDirectory ..\..\dist\nuspec
+nuget pack Axiom.Plugins.ParticleFX.nuspec -OutputDirectory ..\..\dist\nuspec
+nuget pack Axiom.Plugins.SystemDrawingCodecs.nuspec -OutputDirectory ..\..\dist\nuspec
+nuget pack Axiom.SceneManagers.Octree.nuspec -OutputDirectory ..\..\dist\nuspec
+nuget pack Axiom.SceneManagers.PortalConnected.nuspec -OutputDirectory ..\..\dist\nuspec
+nuget pack Axiom.SceneManagers.Bsp.nuspec -OutputDirectory ..\..\dist\nuspec
+
+pause
