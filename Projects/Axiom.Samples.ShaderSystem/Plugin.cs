4e79e49c50f1e0ca3c2065ba7978cf680738220a
Andrés Ambrois <658116+doublea@users.noreply.github.com>
2012-04-05 19:47:55 +0000

    Added ShaderSample showcasing RTSS. Still a WIP

---
+++ Projects/Axiom.Samples.ShaderSystem/Plugin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Axiom.Samples.ShaderSystem
+{
+    [Export(typeof(IPlugin))]
+    public class Plugin : SamplePlugin
+    {
+        private ShaderSample sample;
+
+        public override void Initialize()
+        {
+            sample = new ShaderSample();
+            Name = sample.Metadata["Title"] + " Sample";
+            AddSample(sample);
+        }
+    }
+}
