28e3fbb2b1a0ef356d9157f27763d9c7684c25f3
borrillis <borrillis@bb9bf1cf-960d-0410-9a3a-baeba60cf407>
2012-04-16 21:21:52 +0000

    whoops, should not have renamed this folder

---
+++ Source/Axiom.Framework/Exceptions/IErrorDialog.cs
@@ -0,0 +1,40 @@
+#region MIT/X11 License
+
+//Copyright © 2003-2012 Axiom 3D Rendering Engine Project
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+//
+//The above copyright notice and this permission notice shall be included in
+//all copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//THE SOFTWARE.
+
+#endregion License
+
+using System;
+
+namespace Axiom.Framework.Exceptions
+{
+	/// <summary>
+	/// Interface for a display of error
+	/// </summary>
+	public interface IErrorDialog
+	{
+		/// <summary>
+		/// Causes the exception to be displayed on the screen
+		/// </summary>
+		/// <param name="exception">The exception to display</param>
+		void Show( Exception exception );
+	}
+}
