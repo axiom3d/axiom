e93f7f5964c9f20807ef36223d97f416f5ee7fe4
Andrés Ambrois <658116+doublea@users.noreply.github.com>
2012-05-02 04:36:33 +0000

    Further work on GLSL ES

---
+++ Source/Core/Axiom.RenderSystems.OpenGLES2/GLSLES/GLSLESGpuProgram.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Axiom.RenderSystems.OpenGLES2.GLSLES
+{
+    class GLSLESGpuProgram
+    {
+        public GLSLESProgram GLSLProgram { get; set; }
+
+        public Graphics.GpuProgramType Type { get; set; }
+    }
+}
