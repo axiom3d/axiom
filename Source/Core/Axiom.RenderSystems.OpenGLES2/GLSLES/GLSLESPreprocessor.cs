50069f3f5742cd49e472534e4ff5d51cc635f5cf
Michael Cummings <757221+borrillis@users.noreply.github.com>
2012-05-07 18:09:08 +0000

    [GLES2]     Updated License     Updated SV File Info sections     Added Namespace regions where needed

--- Source/Core/Axiom.RenderSystems.OpenGLES2/GLSLES/GLSLESPreprocessor.cs
+++ Source/Core/Axiom.RenderSystems.OpenGLES2/GLSLES/GLSLESPreprocessor.cs
@@ -1,3 +1,38 @@
+#region MIT/X11 License
+
+//Copyright © 2003-2012 Axiom 3D Rendering Engine Project
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+//
+//The above copyright notice and this permission notice shall be included in
+//all copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//THE SOFTWARE.
+
+#endregion License
+
+#region SVN Version Information
+
+// <file>
+//     <license see="http://axiom3d.net/wiki/index.php/license.txt"/>
+//     <id value="$Id$"/>
+// </file>
+
+#endregion SVN Version Information
+
+#region Namespace Declarations
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +45,8 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 
+#endregion Namespace Declarations
+			
 namespace Axiom.RenderSystems.OpenGLES2.GLSLES
 {
 	/// <summary>
