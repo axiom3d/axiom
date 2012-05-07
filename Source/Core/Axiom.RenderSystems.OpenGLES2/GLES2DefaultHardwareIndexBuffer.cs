de2a73257d90d22810c858d2f966d3847ae3453b
Michael Cummings <757221+borrillis@users.noreply.github.com>
2012-05-07 15:26:39 +0000

    [GLES2] Fixed compiler errors, updated to use BufferBase instead of IntPtr

--- Source/Core/Axiom.RenderSystems.OpenGLES2/GLES2DefaultHardwareIndexBuffer.cs
+++ Source/Core/Axiom.RenderSystems.OpenGLES2/GLES2DefaultHardwareIndexBuffer.cs
@@ -1,4 +1,5 @@
 #region LGPL License
+
 /*
 Axiom Graphics Engine Library
 Copyright (C) 2003-2010 Axiom Project Team
@@ -24,137 +25,133 @@ You should have received a copy of the GNU Lesser General Public
 License along with this library; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 */
+
 #endregion LGPL License
 
 #region SVN Version Information
+
 // <file>
 //     <license see="http://axiomengine.sf.net/wiki/index.php/license.txt"/>
 //     <id value="$Id: GLES2DefaultHardwareIndexBuffer.cs 2805 2011-08-17 16:26:56Z borrillis $"/>
 // </file>
+
 #endregion SVN Version Information
 
 #region Namespace Declarations
+
 using System;
-using Axiom.Graphics;
+
 using Axiom.Core;
+using Axiom.Graphics;
 using Axiom.Utilities;
-using System.Runtime.InteropServices;
+
 #endregion Namespace Declarations
 
 namespace Axiom.RenderSystems.OpenGLES2
 {
 	/// <summary>
-	/// 
 	/// </summary>
 	[OgreVersion( 1, 8, 0, "It's from trunk rev.'b0d2092773fb'" )]
 	public class GLES2DefaultHardwareIndexBuffer : HardwareIndexBuffer
 	{
 		protected byte[] _data;
-		protected IntPtr _dataPtr;
+		protected BufferBase _dataPtr;
 
 		/// <summary>
-		/// 
 		/// </summary>
-		/// <param name="idxType"></param>
-		/// <param name="numIndexes"></param>
-		/// <param name="usage"></param>
+		/// <param name="idxType"> </param>
+		/// <param name="numIndexes"> </param>
+		/// <param name="usage"> </param>
 		public GLES2DefaultHardwareIndexBuffer( IndexType idxType, int numIndexes, BufferUsage usage )
-			: base( null, idxType, numIndexes, usage, true, false )// always software, never shadowed
+			: base( null, idxType, numIndexes, usage, true, false ) // always software, never shadowed
 		{
 			if ( idxType == IndexType.Size32 )
 			{
 				throw new AxiomException( "32 bit hardware buffers are not allowed in OpenGL ES." );
 			}
 
-			_data = new byte[ sizeInBytes ];
-			_dataPtr = Memory.PinObject( _data );
+			this._data = new byte[ sizeInBytes ];
+			this._dataPtr = BufferBase.Wrap( this._data );
 		}
+
 		/// <summary>
-		/// 
 		/// </summary>
-		/// <param name="offset"></param>
-		public IntPtr GetData( int offset )
-		{
-			return new IntPtr( _dataPtr.ToInt32() + offset );
-		}
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <param name="offset"></param>
-		/// <param name="length"></param>
-		/// <param name="locking"></param>
-		/// <returns></returns>
+		/// <param name="offset"> </param>
+		/// <param name="length"> </param>
+		/// <param name="locking"> </param>
+		/// <returns> </returns>
 		protected override BufferBase LockImpl( int offset, int length, BufferLocking locking )
 		{
 			// Only for use internally, no 'locking' as such
-			return GetData( offset );
+			return this._dataPtr + offset;
 		}
+
 		/// <summary>
-		/// 
 		/// </summary>
 		protected override void UnlockImpl()
 		{
 			// Nothing to do
 		}
+
 		/// <summary>
-		/// 
 		/// </summary>
-		/// <param name="offset"></param>
-		/// <param name="length"></param>
-		/// <param name="locking"></param>
-		/// <returns></returns>
+		/// <param name="offset"> </param>
+		/// <param name="length"> </param>
+		/// <param name="locking"> </param>
+		/// <returns> </returns>
 		public override BufferBase Lock( int offset, int length, BufferLocking locking )
 		{
 			isLocked = true;
-			return GetData( offset );
+			return this._dataPtr + offset;
 		}
+
 		/// <summary>
-		/// 
 		/// </summary>
 		public override void Unlock()
 		{
 			isLocked = false;
 			// Nothing to do
 		}
+
 		/// <summary>
-		/// 
 		/// </summary>
-		/// <param name="offset"></param>
-		/// <param name="length"></param>
-		/// <param name="dest"></param>
+		/// <param name="offset"> </param>
+		/// <param name="length"> </param>
+		/// <param name="dest"> </param>
 		public override void ReadData( int offset, int length, BufferBase dest )
 		{
 			Contract.Requires( ( offset + length ) <= sizeInBytes );
-			Memory.Copy( GetData( offset ), dest, length );
+
+			Memory.Copy( dest, this._dataPtr + offset, length );
 		}
+
 		/// <summary>
-		/// 
 		/// </summary>
-		/// <param name="offset"></param>
-		/// <param name="length"></param>
-		/// <param name="src"></param>
-		/// <param name="discardWholeBuffer"></param>
+		/// <param name="offset"> </param>
+		/// <param name="length"> </param>
+		/// <param name="src"> </param>
+		/// <param name="discardWholeBuffer"> </param>
 		public override void WriteData( int offset, int length, BufferBase src, bool discardWholeBuffer )
 		{
 			Contract.Requires( ( offset + length ) <= sizeInBytes );
 			// ignore discard, memory is not guaranteed to be zeroised
-			Memory.Copy( src, GetData( offset ), length );
+			Memory.Copy( src, this._dataPtr + offset, length );
+
 		}
 
 		/// <summary>
-		/// 
 		/// </summary>
-		/// <param name="disposeManagedResources"></param>
+		/// <param name="disposeManagedResources"> </param>
 		protected override void dispose( bool disposeManagedResources )
 		{
 			if ( !IsDisposed )
 			{
 				if ( disposeManagedResources )
 				{
-					if ( _data != null )
+					if ( this._data != null )
 					{
-						Memory.UnpinObject( _data );
-						_data = null;
+						this._dataPtr.SafeDispose();
+						this._data = null;
 					}
 				}
 			}
@@ -165,4 +162,3 @@ namespace Axiom.RenderSystems.OpenGLES2
 		}
 	}
 }
-
