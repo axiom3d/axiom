a442b01f6f14152d538b800b394581d45ae7a8df
Michael Cummings <757221+borrillis@users.noreply.github.com>
2012-05-17 20:29:18 +0000

    [SAMPLES, DROID]   Added subclass of AndroidGameView

--- Source/Samples/Axiom.Samples.Browser/Main.Droid.cs
+++ Source/Samples/Axiom.Samples.Browser/Main.Droid.cs
@@ -19,28 +19,33 @@ namespace Axiom.Samples.Browser
 	[Activity( Label = "Axiom.Samples.Browser", MainLauncher = true, Icon = "@drawable/GameThumbnail" )]
 	public class Main : Activity
 	{
-		SampleBrowser browser = new SampleBrowser();
-
 		protected override void OnCreate( Bundle bundle )
 		{
 			base.OnCreate( bundle );
 			Axiom.Platform.Android.Plugin platform = new Plugin();
 			Axiom.RenderSystems.OpenGLES2.GLES2Plugin renderSystem = new GLES2Plugin();
 
-			browser.Go();
-		}
+            // Inflate our UI from its XML layout description
+            // - should match filename res/layout/main.xml ?
+            SetContentView(Resource.Layout.Main);
+
+            // Load the view
+            FindViewById(Resource.Id.samplebrowserview);
+        }
 
 		protected override void OnPause()
 		{
 			base.OnPause();
-			browser.PauseCurrentSample();
+            var view = FindViewById<SampleBrowserView>(Resource.Id.samplebrowserview);
+            view.Pause();
 		}
 
 		protected override void OnResume()
 		{
 			base.OnResume();
-			browser.UnpauseCurrentSample();
-		}
+            var view = FindViewById<SampleBrowserView>(Resource.Id.samplebrowserview);
+            view.Resume();
+        }
 
 	}
 }
