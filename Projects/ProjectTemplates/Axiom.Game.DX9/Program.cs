281f0d213345bb9a2fa832b89645ada109aadff9
Michael Cummings <757221+borrillis@users.noreply.github.com>
2012-07-18 19:52:59 +0000

    [TEMPLATES] Added First Visual Studio 2010 Project Template (DirectX9)

---
+++ Projects/ProjectTemplates/Axiom.Game.DX9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+$if$ ($targetframeworkversion$ >= 3.5)using System.Linq;
+$endif$using System.Text;
+
+namespace $safeprojectname$
+{
+	internal class Program
+	{
+		private static void Main()
+		{
+			try
+			{
+				System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US", false);
+				(new Game()).Run();            
+			}
+			catch (Exception ex)
+			{
+				System.Diagnostics.Debug.WriteLine(Axiom.Core.LogManager.BuildExceptionString(ex));
+			}
+		}
+	}
+}
