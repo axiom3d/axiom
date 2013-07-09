6a858869a005e15beebff769aa8a8a371c22f17d
Michael Cummings <757221+borrillis@users.noreply.github.com>
2013-07-09 19:48:00 -0400

    Added OSX Platform Manager

---
+++ Source/Core/Axiom.Platforms.OSX/OSXPlatformManager.cs
@@ -0,0 +1,24 @@
+﻿#region Namespace Declarations
+
+using System.ComponentModel.Composition;
+using Axiom.Core;
+using Axiom.Input;
+
+#endregion Namespace Declarations
+
+namespace Axiom.Platforms.Linux
+{
+	[Export( typeof ( IPlatformManager ) )]
+	public class LinuxPlatformManager : IPlatformManager
+	{
+		public LinuxPlatformManager()
+		{
+			LogManager.Instance.Write( "Linux Platform Manager Loaded." );
+		}
+
+		public void Dispose()
+		{
+			LogManager.Instance.Write( "Linux Platform Manager Shutdown." );
+		}
+	}
+}
