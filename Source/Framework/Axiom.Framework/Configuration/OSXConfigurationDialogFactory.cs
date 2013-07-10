386624a974703e9e919e848c1d29f792828ac430
Michael Cummings <757221+borrillis@users.noreply.github.com>
2013-07-10 17:53:39 -0400

    Added OSX ConfigurationManager implementation Added OSX ConfigurationDialog

--- Source/Framework/Axiom.Framework/Configuration/OSXConfigurationDialogFactory.cs
+++ Source/Framework/Axiom.Framework/Configuration/OSXConfigurationDialogFactory.cs
@@ -22,42 +22,23 @@
 
 #endregion License
 
-using System;
+using Axiom.Core;
 
 namespace Axiom.Framework.Configuration
 {
-	public class ConfigurationManagerFactory
+	internal class OSXConfigurationDialogFactory : ConfigurationDialogFactory
 	{
-		public static IConfigurationManager CreateDefault()
+		#region Overrides of ConfigurationDialogFactory
+
+		/// <summary>
+		/// Create an instance of the ConfigurationDialog
+		/// </summary>
+		/// <returns></returns>
+		public override IConfigurationDialog CreateConfigurationDialog( Root engine, ResourceGroupManager resourceManager )
 		{
-#if OSX
-			return new OSXConfigurationManager();
-#else
-			var platform = Environment.OSVersion.Platform;
-			switch ( platform )
-			{
-				case PlatformID.Xbox:
-					return new XBoxConfigurationManager();
-#if !(XBOX || XBOX360 || WINDOWS_PHONE)
-				case PlatformID.MacOSX:
-#endif
-				case PlatformID.Unix:
-#if SILVERLIGHT && WINDOWS_PHONE
-				case PlatformID.NokiaS60:
-#endif
-				case PlatformID.WinCE:
-					return new XBoxConfigurationManager();
-				case PlatformID.Win32NT:
-				case PlatformID.Win32S:
-				case PlatformID.Win32Windows:
-				default:
-#if !(XBOX || XBOX360 || WINDOWS_PHONE || SILVERLIGHT || ANDROID)
-					return new DefaultConfigurationManager();
-#else
-					return null;
-#endif
-			}
-#endif
+			return new OSXConfigurationDialog( engine, resourceManager );
 		}
+
+		#endregion
 	}
 }
