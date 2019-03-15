e06277197d4f93e551fa8cb18a036edc403a4684
Michael Cummings <cummings.michael@live.com>
2019-03-15 17:40:52 -0400

    Convert DirectX9 RenderSystem plugin to Sdk Project style

--- Source/Core/Axiom.RenderSystems.DirectX9/D3D9Plugin.cs
+++ Source/Core/Axiom.RenderSystems.DirectX9/D3D9Plugin.cs
@@ -37,7 +37,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 #region Namespace Declarations
 
-using System.ComponentModel.Composition;
+using System.Composition;
 using Axiom.Core;
 using Axiom.Graphics;
 
