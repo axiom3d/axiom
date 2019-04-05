80750bc34e64b19a7c79678644cbaf5fe161efe4
Michael Cummings <cummings.michael@live.com>
2019-04-05 17:14:47 -0400

    Update Samples

---
+++ Source/Samples/Axiom.Samples.Browser.Shared/Main.Droid.cs
@@ -0,0 +1,52 @@
+﻿#region Namespace Declarations
+
+using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+using Axiom.Platform.Android;
+using Axiom.RenderSystems.OpenGLES2;
+
+#endregion Namespace Declarations
+			
+namespace Axiom.Samples.Browser
+{
+	[Activity( Label = "Axiom.Samples.Browser", MainLauncher = true, Icon = "@drawable/GameThumbnail" )]
+	public class Main : Activity
+	{
+		protected override void OnCreate( Bundle bundle )
+		{
+			base.OnCreate( bundle );
+			Axiom.Platform.Android.Plugin platform = new Plugin();
+			Axiom.RenderSystems.OpenGLES2.GLES2Plugin renderSystem = new GLES2Plugin();
+
+            // Inflate our UI from its XML layout description
+            // - should match filename res/layout/main.xml ?
+            SetContentView(Resource.Layout.Main);
+
+            // Load the view
+            FindViewById(Resource.Id.samplebrowserview);
+        }
+
+		protected override void OnPause()
+		{
+			base.OnPause();
+            var view = FindViewById<SampleBrowserView>(Resource.Id.samplebrowserview);
+            view.Pause();
+		}
+
+		protected override void OnResume()
+		{
+			base.OnResume();
+            var view = FindViewById<SampleBrowserView>(Resource.Id.samplebrowserview);
+            view.Resume();
+        }
+
+	}
+}
+
---
+++ Source/Samples/Axiom.Samples.Browser.Win32/Program.cs
@@ -0,0 +1,61 @@
+﻿#region MIT/X11 License
+
+//Copyright © 2003-2012 Axiom 3D Rendering Engine Project
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+//
+//The above copyright notice and this permission notice shall be included in
+//all copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//THE SOFTWARE.
+
+#endregion License
+
+using System;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Axiom.Framework.Exceptions;
+
+namespace Axiom.Samples
+{
+	internal static class Program
+	{
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool SetDllDirectory(string path);
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+		private static void Main()
+		{
+			try
+			{
+                Thread.CurrentThread.CurrentCulture = new CultureInfo( "en-US", false );
+				using ( var sb = new SampleBrowser() )
+				{
+					sb.Go();
+				}
+			}
+			catch ( Exception ex )
+			{
+				IErrorDialog messageBox = new WinFormErrorDialog();
+				messageBox.Show( ex );
+			}
+		}
+	}
+}
